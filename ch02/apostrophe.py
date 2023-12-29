722590c9f64addaefdc28854b7a8127e8356462e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 11:04:04 -0500

    added a file

---
+++ ch02/apostrophe.py
@@ -0,0 +1,6 @@
+# Using single and double quotes correctly
+#apostrophe appears inside a set of double quotes
+# syntax error: if you use single quotes, python can't identify where the string should end
+message = 'One of Python's strength is its diverse community.'
+print(message)
+
