97387ee649797505dc46ded9a023b3f522d1718d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 23:34:18 -0500

    added a file

---
+++ ch02/famous_quote_2.py
@@ -0,0 +1,6 @@
+# Exercise 2-6: Repeat Exercise 2-5, but this time, represent the famous person's name using a variable called famous_person
+# Then compose your message and represent it with a new variable called message
+# Print your message
+famous_person = "albert einstein"
+message = "The greatest glory in living lies not in never falling, but in rising every time we fall."
+print(f'{famous_person.title()} once said, "{message}"')
