42455033e7d5b88e0bdf8f0aa6f0340802deaf86
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 00:12:41 -0500

    added a file

---
+++ ch03/names.py
@@ -0,0 +1,8 @@
+# Exercise 3-1 Names: store the names of a few of your friends in a list called names
+# Print each person's name by accessing each element in the list, one at a time
+
+names=['jimmy', 'mimi', 'tony', 'nana']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
