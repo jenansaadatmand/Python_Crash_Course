bf996555433051005ff5f9cc423eecd39e7dfd91
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 23:37:58 -0500

    added a file

---
+++ ch02/personal_message.py
@@ -0,0 +1,5 @@
+# Exercise 2-3: Use a variable to represent a person's name, 
+# and print a message to that person
+# your message should be simple, such as, "Hello Eric, would you like to learn some Python today ?"
+person_name = "Jimmy"
+print(f"Hello, {person_name}, would like to learn some Python today ?")
