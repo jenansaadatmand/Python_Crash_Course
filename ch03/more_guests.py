63e893eccdb2683e454c461e84bea130cdad5fb2
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 00:11:26 -0500

    added a file

---
+++ ch03/more_guests.py
@@ -0,0 +1,29 @@
+# Exercise 3-6 More guests: you just found a bigger dinner table, so now
+# more space is available. Think of three guests to invite to dinner
+# Start with your program from exercise 3-4 or 3-5
+# Add a print() call to the end of your program, informing people that you found a bigger table
+# use insert() to add one new guest to the beginning of your list
+# use insert() to add one new guest to the middle of your list
+# use append() to add one new guest to the end of your list
+# Print a new set of invitation messages, one for each person in your list
+
+guest_list = ['jimmy','tiffany', 'spoiler']
+print(f"Hello {guest_list[0].title()}, you are invited for dinner.")
+print(f"Hello {guest_list[1].title()}, you are invited for dinner.")
+print(f"Hello {guest_list[2].title()}, you are invited for diner.")
+print()
+for guest in guest_list: 
+    print(f"Hello {guest.title()}, I have found a bigger table.")
+print()
+guest_list.insert(0, 'nana') # adding one guest to the beginning of list
+print(guest_list)
+guest_list.insert(2, 'soso') # Adding one guest in the middle of list
+print(guest_list)
+guest_list.append('fofo') # appending one guest to the end of the list
+print(guest_list)
+print()
+# printing new set of invitation messages for each guest 
+for guest in guest_list:
+    print(f"Hello {guest.title()}, you are invited to diner.")
+print()
+print(len(guest_list))
