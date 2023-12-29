f46b944ac99a66f7ea35d4ef1cc74010269a5036
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 23:19:00 -0500

    added a file

---
+++ ch01/hello_world.py
@@ -0,0 +1,10 @@
+# Adding a variable named message and associating a value information with it
+# variables are labels assigned to values, containers or boxes to store values
+# avariable references a certain value 
+message = "Hello Python World ! "
+print(message)
+
+# associating a different value to the variable
+message = "Hello Python Crash Course World !"
+print(message)
+
