e65ba6e4d86c47d6038e2216483a82730a126ac7
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 23:35:42 -0500

    added a file

---
+++ ch02/favorite_number.py
@@ -0,0 +1,7 @@
+# Exercise 2-10: Use a variable to represent your favorite number.
+# Then, using that variable, create a message that reveals your favorite number
+# print that message
+# Jenan Saadatmand March 28 2023 this is a comment 
+favorite_num = "333" 
+message = f"My favorite number is {favorite_num}"
+print(message)
