25b548d42f7ab94a6302e18eb4896eddc23d40ea
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 00:08:48 -0500

    added a file

---
+++ ch03/greetings.py
@@ -0,0 +1,9 @@
+# Exercise 3-2: Greetings: start with the list you used in exercise 3-1, but
+# instead of just printing each person's name, print a message to them 
+# The text of each message should be the same, but each message should
+# be personalized with the person's name 
+names=['jimmy', 'mimi', 'tony', 'nana']
+print(f"Hello {names[0].title()}, you are invited to my party !")
+print(f"Hello {names[1].title()}, you are invited to my part !")
+print(f"Hello {names[2].title()}, you are invited to my party !")
+print(f"Hello {names[3].title()}, you are invited to my party !")
