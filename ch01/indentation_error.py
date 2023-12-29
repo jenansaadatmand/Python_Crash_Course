1cc99a1c8cbf9fa945a513e358889f9d30908968
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 11:09:45 -0500

    added a file

---
+++ ch01/indentation_error.py
@@ -0,0 +1,9 @@
+# Exercise 1-2. Hello World Typos: Unexpected indentation error
+# Open the hello_world.py file you just created. Make a typo somewhere in the line and run the program again. 
+# Can you make a typo that generates an error? Can you make sense of the error message? Can you make a typo that doesn’t generate an error? Why do you think it didn’t make an error?
+
+# Answer:
+# I choose to introduce an indentation error:
+
+message = ("Hello World !")
+    print(message)
