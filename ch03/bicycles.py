c5de46bc10d4513732b2414fe62f0dff3565f6ab
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 10:18:56 -0500

    added a file

---
+++ ch03/bicycles.py
@@ -0,0 +1,10 @@
+# Lists are collection of items in a particular order
+bicycles = ['treck', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+# accessing items in list using position or index
+print(bicycles[0])
+print(bicycles[2])
+print(bicycles[3])
+print(bicycles[-1])
+# you can use title(), lower(), and upper() methods on lists items
+print(bicycles[0].title())
