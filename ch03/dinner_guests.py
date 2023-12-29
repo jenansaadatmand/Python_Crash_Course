e21ce6718ec7fca7fae17181b639be3268712589
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-28 23:40:57 -0500

    Update dinner_guests.py
    edited content

--- ch03/dinner_guests.py
+++ ch03/dinner_guests.py
@@ -1,3 +1,3 @@
-# Exercise 3-9 dinner guests: working with one of the programs from excersize 3-4 through 3-7 (pages 41-42)
+# Exercise 3-9 dinner guests: working with one of the programs from exercises 3-4 through 3-7 (pages 41-42)
 # use len() to print a message indicating the number of people you're inviting to dinner.
-# I choose exercise 3-7 rinking guest list and 3-6 more guests  
+# I choose exercises 3-7 shrinking guest list and 3-6 more guests  
