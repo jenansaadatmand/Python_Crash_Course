bf6c2e4db6c2beb6ab1cb9ff308494740cdf7bd5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 10:55:24 -0500

    Delete ch01/simple_messages.py
    deleted it

--- ch01/simple_messages.py
+++
@@ -1,9 +0,0 @@
-# Assign a message to a variable,
-# and print that message
-# Then change the value of the variable to a new message
-# and print the new message
-message = " I am a successful Data Scientist and a python programmer "
-print(message)
-
-message = " I speak fluent French "
-print (message)
