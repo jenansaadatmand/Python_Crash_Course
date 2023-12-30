e93b89601ddb2d04472f2de3f674b6ee570403d6
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:10:36 -0500

    Update guestbook.py

--- ch10/guestbook.py
+++ ch10/guestbook.py
@@ -1,6 +1,6 @@
 # Guestbook: write a while loop that prompts users for their name
 # When they enter their name, print a greeting to the screen
-# and add a line recording their visit in a file called guest_book.txt
+# And add a line recording their visit in a file called guest_book.txt
 # Make sure each entry appears on a new line in the file
 
 filename = 'text_files/guest_book.txt'
