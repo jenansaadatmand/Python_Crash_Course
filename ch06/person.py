cc2bf90657550674ee58f51dca5ba1027afd1fba
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:37:01 -0500

    added new file

---
+++ ch06/person.py
@@ -0,0 +1,16 @@
+# persons: Use a dictionary to store information about a person you know
+# store their first name, last name, age, and the city in which they live
+# you should have keys such as first_name, last_name, age, and city
+# print each piece of information stored in your dictionary
+
+person = {
+    'first_name': 'jimmy', 
+    'last_name': 'saadatmand', 
+    'age': 13, 
+    'city': 'montreal',
+    }
+print(person)  # prints every key-value pair
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
