2e732bc759bf62244f0bf88a73a5763ddc25d572
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:24:22 -0500

    Update division_calculator.py

--- ch10/division_calculator.py
+++ ch10/division_calculator.py
@@ -6,7 +6,7 @@
 
 # Handling the zeroDivisionError exception
 # simple error that causes Python to raise an exception
-# It is impossible to divide a number by zero, but lets ask python to do it anyways
+# It is impossible to divide a number by zero, but let's ask python to do it anyways
 
 #print(5/0)
 # the error ZeroDivisionError reported in the traceback is an exception object
