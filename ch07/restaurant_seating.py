345d07f1cf07b8bd4cfd7868009f7816d5e2685d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:59:24 -0500

    added a file

---
+++ ch07/restaurant_seating.py
@@ -0,0 +1,10 @@
+# Restaurant seating: write a program that asks the user how many people are in their dinner group
+# if the answer is more than eight, print a message saying they'll have to wait for a table.
+# otherwise, report that their table is ready
+
+party_size = input("How many people are in your dinner prty tonight? ")
+party_size = int(party_size)
+if party_size > 8:
+    print("I'am sorry, you will have to wait for a table!")
+else: 
+    print("Your table is ready.")
