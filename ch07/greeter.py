b0ff1cc0cae51cd1c70a080fede9b68714f98b5c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:51:30 -0500

    added a file

---
+++ ch07/greeter.py
@@ -0,0 +1,4 @@
+# Each time you use input() function, need to include a clear prompt for the user using Enter statement
+
+name = input("Please enter your name: ")
+print(f"\nHello {name.title()}!")
