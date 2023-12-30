b25edaa2de7d4d97603c368fe3d50f62c0c71fd4
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:46:33 -0500

    Update pizza_mod.py
    edited spelling

--- ch08/pizza_mod.py
+++ ch08/pizza_mod.py
@@ -1,5 +1,5 @@
-# module contains only the function make_pizza
-# the module name will be pizza_mod
+# Module contains only the function make_pizza
+# The module name will be pizza_mod
 
 def make_pizza(*toppings):
     """Summarize the pizza we are about to make."""
