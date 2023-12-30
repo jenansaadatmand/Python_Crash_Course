eed2979f4b8a88cd8554e7b0f64e7ab5c0442f68
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:55:08 -0500

    added a file

---
+++ ch07/counting.py
@@ -0,0 +1,12 @@
+# While loop runs as long as, or while a certain condition is true
+# the for loop takes a collection of items and executes a block of code once for each item in the collection.
+# use while loop to count up through a series of numbers
+# program of while loop counts from 1 to 5
+
+current_number = 1 # assigning variable to 1 to start counting
+while current_number <=5: # set while loop to keep running as long as the value of current_number is less than or equal to 5
+    print(current_number) # prints the value in the variable 
+    current_number += 1 # increment/add by 1 each time you go through the loop
+# += operator is shorthand for current_number = current_number + 1.
+# Python repeats the loop as long as the condition current_number <= 5 is true.
+# Once the value of current_number is greater than 5, the loop stops running and the program ends
