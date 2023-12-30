41631e47577f4bdabff92b4a9a6ce31582a3ccc1
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:42:01 -0500

    Update seeing_the_world.py

--- ch03/seeing_the_world.py
+++ ch03/seeing_the_world.py
@@ -33,7 +33,7 @@ print(places)
 
 # Notes:
 
-# use sort() to print your list in alphabetical order without modifying the actual list.
+# Use sort() to print your list in alphabetical order without modifying the actual list.
 # show that your list is still in its original order by printing it.
 # use sorted() to print your list in reverse-alphabetical order without changing the order of the original list
 # show that your list is still in its original order by printing it again
