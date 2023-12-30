9b44fa22c53b69e4f511d16d3b9e10a108e1c947
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 03:10:22 -0500

    Update remember_me_refactoring.py

--- ch10/remember_me_refactoring.py
+++ ch10/remember_me_refactoring.py
@@ -1,5 +1,5 @@
 # Refactoring: breaking up code into a series of functions. improving the code by breaking it up into a series of functions that have specific jobs
-# refactoring makes code cleaner, easier to understand and to extend
+# Refactoring makes code cleaner, easier to understand and to extend
 # let's refactor remember_me.py on p206 by moving the bulk of its logic into one or more functions
 # focus of remember_me.py is on greeting the user, 
 # so let's move all of our existing code into a function called greet_user()
