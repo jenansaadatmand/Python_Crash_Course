e1f026c372fc14ade655ea0577c7bde291e5a16e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 01:41:50 -0500

    Update write_message_p193.py

--- ch10/write_message_p193.py
+++ ch10/write_message_p193.py
@@ -1,5 +1,5 @@
 # Append mode: 'a' is used when you want to add content to a file instead of writing over existing content.
-# python does not erase the contents of the file before running the file object, lines will be added to the end of the file
+# Python does not erase the contents of the file before running the file object, lines will be added to the end of the file
 # modifying write_message.py by adding some new reasons we love programming to the existing file programming.txt
 filename = 'text_files/programming.txt'
 with open(filename, 'a') as file_object:  # use the 'a' argument to open the file for appending rather than writing over the existing file.
