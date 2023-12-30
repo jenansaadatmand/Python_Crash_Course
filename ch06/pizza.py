00c8207f60149c6655f987c7b4d2799ba0c832f4
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:39:33 -0500

    Update pizza.py
    edited content

--- ch06/pizza.py
+++ ch06/pizza.py
@@ -1,16 +1,16 @@
 # A list in a dictionary
 # Describe a pizza that someone is ordering
-# two kinds of information are stored for each pizza: a type of crust and a list of toppings
-# list of toppings is a value associated with the key 'toppings'
+# Two kinds of information are stored for each pizza: a type of crust and a list of toppings
+# List of toppings is a value associated with the key 'toppings'
 
-# store information about a pizza being ordered.
+# Store information about a pizza being ordered.
 pizza = {
         'crust': 'thick',
         'toppings': ['mushrooms', 'extra cheese'],
         }
-# summarize the order.
+# Summarize the order.
 print(f"You ordered a {pizza['crust']}-crust pizza" 
     " with the following toppings:") # breaking a long line and end the line with a quotation mark, indent the next line, add an opening quotation mark and continue the string
-# to print the toppings, we use a for loop and access the dictionary using the key toppings
+# To print the toppings, we use a for loop and access the dictionary using the key toppings
 for topping in pizza['toppings']:
     print("\t" + topping)
