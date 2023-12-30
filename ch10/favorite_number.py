bd30a91fcb0e52df2a386af00753199eb55f997f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 03:16:21 -0500

    added a file

---
+++ ch10/favorite_number.py
@@ -0,0 +1,23 @@
+# favourite number: write a program that prompts for the user's favourite number
+# use json.dump() to store this number in a file
+# write a separate program that reads in this value and prints the message, "I know your favourite number! it's -----"
+
+# program that prompts for user favorite number
+#import json
+
+#filename = 'favorite_num.json'
+#num = input("What is your favourite number? ")
+
+#with open(filename, 'w') as f:
+#    json.dump(num, f)
+#    print("Thanks! I'll rememmber that.")
+
+
+# program that Reads user's favourite number
+import json
+filename = 'favorite_num.json'
+with open(filename) as f:     
+   num = json.load(f)
+print(f"I know your favorite number! it's {num}.")
+
+# i forgot the f before " and it did not replace the number in line 21 when printed
