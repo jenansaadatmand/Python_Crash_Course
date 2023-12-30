e039c83eeef281591afdf8b33acbe318ab7780c3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:08:23 -0500

    added new file

---
+++ ch05/conditional_tests.py
@@ -0,0 +1,30 @@
+# Conditional tests: write a series of conditional tests
+# Print a statement describing each test and your prediction for the results of each test.
+# Your code should look somethig like this:
+
+#car = 'subaru'
+#print("Is car == 'subaru' ? I predict True.")
+#print(car == 'subaru')
+#print("\nIs car == 'audio' ? I predict False.")
+#print(car == 'audi')
+# Look closely at your results, and make sure you understand why each line evaluates to True or False
+# Create at least ten tests. 
+# Have at least five tests evaluate to True and another five tests evaluate to False
+
+motorcycle = 'suzuki'
+print("Is motorcycle == 'suzuki'? I predict True.")
+print(motorcycle == 'suzuki')
+print("Is motorcycle != 'suzuki' ? I predict False.")
+print(motorcycle != 'suzuki') 
+print("\nmotorcycle == 'harley' ? I predict False.")
+print(motorcycle == 'harley')
+print("motorcycle != 'harely' ? I predict True.")
+print(motorcycle != 'harely')
+print("\nmotorcycle == 'honda' ? I predict False.")
+print(motorcycle == 'honda')
+print("motorcycle != 'honda ? I predict True.")
+print(motorcycle != 'honda')
+print("\nmotorcycle == 'haya busa' ? I predict False.")
+print(motorcycle == 'haya busa')
+print("motorcycle != 'haya busa' ? I predict True.")
+print(motorcycle != 'haya busa')
