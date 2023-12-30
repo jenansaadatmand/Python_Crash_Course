2330b1b6af5a47b963f136b791dcf58af00e874c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:48:53 -0500

    Update organizing_lists.py

--- ch03/organizing_lists.py
+++ ch03/organizing_lists.py
@@ -5,6 +5,6 @@
 # Sorting lists temporarily using sorted() method, the original list does not change
 # Sorted(cars, reverse=True) displays in reverse alphabetical order and temporarily
 # Printing a list in reverse order permanently, i.e. to reverse the original order of a list
-# using reverse() method (not in alphabetical order)
+# Using reverse() method (not in alphabetical order)
 # apply reverse() method a second time to revert back to the original order in the list from the reversed order in the list
 # len(list) displays the length of the list
