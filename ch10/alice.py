0d0ec7e386a152a4abf191e8c0aa17e793dfc0bb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:15:20 -0500

    added a file

---
+++ ch10/alice.py
@@ -0,0 +1,18 @@
+# Handling the FileNotFoundError Exception: using a try-except block
+# Handling missing files, different location, misspelled filename or does not exist at all
+# let's try to read a file that does not exist
+# program tries to read in the contents of Alice in wonderland, but we have'nt saved the file alice.txt in the same directory as alice.py
+
+# use of f to represent the file object, common convension
+# encoding argunent needed when your system's default encoding doesn't match the encoding of the file that's being read
+# UTF-8 is a variable-width character encoding used for electronic communication. Defined by the Unicode Standard, the name is derived from Unicode (or Universal Coded Character Set) Transformation Format – 8-bit. ... Code points with lower numerical values, which tend to occur more frequently, are encoded using fewer bytes.
+# UTF-8 is the most widely used way to represent Unicode text in web pages, and you should always use UTF-8 when creating your web pages and databases
+# Python cannot read from a missing file, so it raises an exception
+# traceback reports a FileNotFoundError exception is created when python cannot find the file it's trying to open
+filename = 'alice.txt'
+
+try:  # the code in the try block produces an error so python looks for an except block that matches the error
+    with open(filename, encoding = 'utf-8'):
+        f.read()
+except FileNotFoundError: # python runs the except block to handle the error
+    print(f"sorry, the file {filename} doesn't exist.") # result in friendly error message is printed to the user instead of the traceback
