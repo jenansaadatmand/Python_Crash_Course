329bec56aaa2972dd5697631f39bbb3092a4f8bd
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:43:12 -0500

    added a file

---
+++ ch10/addition_calculator.py
@@ -0,0 +1,22 @@
+# addition calculator: wrap your code from Excersize 10-6 in a while loop so the user can continue entering numbers even if they make a mnistake and enter text instead.
+
+print("Enter 'q' to quit.\n")
+
+while True:
+    try:
+      x = input("\nGive me a number: ")
+      if x == 'q':
+        break
+      x = int(x)
+
+      y = input("Give me another number: ")
+      if y == 'q':
+        break
+
+      y = int(y)
+    
+    except ValueError:
+        print("I really need a number.")
+    else:
+        sum = x + y
+        print(f"The sum of (x) and (y) is {sum}.")
