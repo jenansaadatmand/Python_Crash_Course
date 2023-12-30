6bb15b978e9519a3b503f96b4e3875f26a831e85
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:24:32 -0500

    added a file

--- ch04/magicians.py
+++ ch04/magicians.py
@@ -1,18 +1,22 @@
-# for loop : to do the same action to each item within a list
-magicians = ['alice', 'david', 'carolina']
+# Looping is important to automate repetitive tasks
+# iteration with a for loop: (instead of retrieving or accessing each name one by one using index magicians[0] etc)
 # for every magician in the list of magicians, print the magician's name
-for magician in magicians: # pull a name from the list and associate it to the variable magician or x
-    print(magician.title()) # print the name that just been assigned to magician
-print()
-# less efficient solution 2:
-print(magicians[0].title())
-print(magicians[1].title())
-print(magicians[2].title())
-print()
 
-# composing a message usinf f-string to each magician
-for magician in magicians:
-    print(f"{magician.title()}, that was a great trick !")
-    print(f"I can't wait to see your next trick, {magician.title()}.\n")
-print("Thank you, everyone. That was a great magic show!") # This print() is outside of the loop
+magicians = ["alice", "david", "carolina" ] # defining a list
+print(magicians)
 
+for magician in magicians:  # Defining a for loop, pull a name from the list and associate it with temporary variable magician then title it
+    print(magician.title()) # print the names asscoiated with the variable magician 
+# python will repeat the last two lines, iterating through the list
+
+for magician in magicians: # add as many repetitive tasks under the for loop
+    print(f"\n{magician.title()}, that was a great trick!")
+    print(f"I can't wait for your next trick {magician.title()}.\n") # \n adds a new line after each pass through the loop creating  as set of neatly grouped messages 
+print("Thank you, everyone. That was a great magic show!")
+
+print()
+# solution 2: replace magician with i or any letter x etc
+
+for i in magicians: # define a for loop, pull a name from list and associate it with temporary variable i  
+    print(i.title()) # print the names associated with i  
+# python will repeat the last two lines, iterating through the list 
