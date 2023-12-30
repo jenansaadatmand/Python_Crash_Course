ed3ec22ce566a9eb231e3c7b5784300095b22956
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 00:25:07 -0500

    added a file

---
+++ ch09/my_cars.py
@@ -0,0 +1,12 @@
+# importing multiple Classes from a Module
+# if we want to make a regular car and an electric car (two instances) in the same file, import both classes Car and ElectricCar
+
+from car import Car, ElectricCar  # separate each class with a commma
+
+my_beetle = Car('volkswagen', 'beetle', 2019)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = ElectricCar('tesla', 'roadster', 2019)
+print(my_tesla.get_descriptive_name())
+
+
