1778be0fed9534b34a6b5261f444f1d13990480d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:30:56 -0500

    added a file

---
+++ ch03/printing_statement_using_pop.py
@@ -0,0 +1,17 @@
+# Using pop() method to print a statement
+# Removing the last item in the list and storing it in a variable to be used later
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+last_owned = motorcycles.pop()
+print(last_owned)
+print(f'The last motorcycle I owned was a {last_owned.title()}.')
+print()
+
+# using pop() to remove an item from any position in a list by including the index of the item you to be removed in paranthesis
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles.pop(1))
+first_owned = motorcycles.pop(0)
+print(f'The first motorcycle I owned was a {first_owned.title()}')
+print(motorcycles)
