4a6abe43cf2d82a646c665d2f9d601546c40a4d9
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:11:47 -0500

    Update first_numbers.py

--- ch04/first_numbers.py
+++ ch04/first_numbers.py
@@ -56,7 +56,7 @@ print()
 
 
 # List comprehension: to generate lists using one line
-# it combines a for loop and the creation of a new elements in one line
+# It combines a for loop and the creation of a new elements in one line
 # and automatically appends each new element
 # building a list of square numbers using list comprehension: 
 squares = [value **2 for value in range(1, 11)]
