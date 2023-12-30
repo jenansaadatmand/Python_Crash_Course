7fcf58db62a9d020f3d1cbf3696d304230c7f86f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:13:55 -0500

    Update alice_p198.py

--- ch10/alice_p198.py
+++ ch10/alice_p198.py
@@ -3,7 +3,7 @@
 # Pull alice in wonderland book from project Gutenberg
 # try to count the number of words in the text
 # use a string method split(), which build a list of words from a string
-# string() method separates a string into parts wherever it finds a space and stores all the parts of a string in a list
+# string() method separates a string into parts wherever it finds space and stores all the parts of a string in a list
 # to count the number of words, we will use split() to separate string into words in a list then count them with len()
 title = 'Alice in wonderland'
 list_of_words = title.split()
