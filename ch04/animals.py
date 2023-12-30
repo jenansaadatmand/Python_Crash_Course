52878b063a1d807fa6e7d2987613e31d857c690f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:46:45 -0500

    Update animals.py
    edited a word

--- ch04/animals.py
+++ ch04/animals.py
@@ -1,5 +1,5 @@
 # Exercise 4-2 animals: Think of at least three different that have a common characteristic
-#Store the names of these animals in a list, and then use a for loop to 
+# Store the names of these animals in a list, and then use a for loop to 
 # Print out the name of each animal
 # Modify your program to print a statement about each animal, such as 
 # A dog would make a great pet
