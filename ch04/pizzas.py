8f79cba195a0b54132440c4f84f05875bccf4a3c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:38:45 -0500

    Update pizzas.py
    edited content

--- ch04/pizzas.py
+++ ch04/pizzas.py
@@ -1,13 +1,13 @@
 # Exercise 4-1 Pizzas: Think of at least three kinds of your facvorite pizza. 
-# store these pizza names in a list, and 
-# then use a for loop to print the name of each pizza
+# Store these pizza names in a list, and 
+# Then use a for loop to print the name of each pizza
 # Modify your for loop to print a sentence using the name of the pizza,
 # instead of printing just the name of the pizza.
-# for each pizza, you should have one line of output 
+# For each pizza, you should have one line of output 
 # containing a simple statement like I like pepperoni pizza
-# add a line at the end of your program, outside the for loop, 
+# Add a line at the end of your program, outside the for loop, 
 # that states how much you like pizza.
-# the output should consist of three or more lines about the kinds 
+# The output should consist of three or more lines about the kinds 
 # of pizza you like and then an additional sentence , such as I really love pizza !
 
 
