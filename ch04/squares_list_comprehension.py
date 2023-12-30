5fc3422fa827b8f897426b44f5a19b968a277490
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:57:09 -0500

    added new file

---
+++ ch04/squares_list_comprehension.py
@@ -0,0 +1,7 @@
+# list comprehension of squares.py used instead of long code multistep code using range() and for loop and append method to make list numbers  
+# list comprehension in one line of code, it combines for loop and the creation of new elements into one line and automatically appends each new element
+# square numbers is presented as value ** 2
+
+squares = [value**2 for value in range(1, 11)]
+print(squares)
+print("\n")
