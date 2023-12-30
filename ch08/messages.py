b25d9835603675395bbf1ff141119598904e6768
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:31:09 -0500

    added a file

---
+++ ch08/messages.py
@@ -0,0 +1,24 @@
+# exercise 8-9 messages: make a list containig a series of short text messages.
+# pass the list to a function called show_messages(), which print each text message
+
+messages = ['I love you', 'Hello, how are you', 'I love Python']
+
+def show_messages(messages):
+    """Prints all text message in a list."""
+    for message in messages:
+        print(message)
+
+show_messages(messages) 
+
+print("\n")
+
+# solution 2
+short_msgs = ['I love you', 'Hello, how are you?', 'I love Python']
+
+def show_messages(short_msgs):
+    """Displays all text message in a list"""
+    for msg in short_msgs:
+        print(msg)
+
+show_messages(short_msgs)
+
