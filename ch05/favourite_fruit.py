f0585df9de439ff53c12d6d009f3ef9640639806
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:18:23 -0500

    added new file

---
+++ ch05/favourite_fruit.py
@@ -0,0 +1,18 @@
+# Favourite fruit: Make a list of your favourite fruits
+# and then write a series of independent if statements that check for certain fruits in your list
+# Make a list of your three favourite fruits and call it favourite_fruits
+# Write five if statements 
+# Each should check whether a certain kind of fruit is in your list
+# If the fruit is in your list, the if block should print a statement such as You really like bananas !
+
+favourite_fruits = ['blueberries', 'bananas', 'pomegranates']
+if 'blueberries' in favourite_fruits:
+    print("You really like blueberries!")  
+if 'bananas' in favourite_fruits:
+    print("You really like bananas!")
+if 'oranges' in favourite_fruits:
+    print("You really like oranges!")
+if 'pomegranates' in favourite_fruits:
+    print("You really like pomegranates!")
+if 'mandarines' in favourite_fruits:
+    print("You really like mandarines!")
