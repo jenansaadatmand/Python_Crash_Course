8239f98ef8b9347948fb78f3d9ca9526dda4c630
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:52:56 -0500

    Update cities.py
    edited spelling

--- ch07/cities.py
+++ ch07/cities.py
@@ -1,7 +1,7 @@
-# using break to exit a loop
+# Using break to exit a loop
 # To exit a while loop immediately without running any remaining code in the loop, regardless of the results of any conditional test, use the break statement.
-# break statement directs the flow of your program; you can use it to control which lines of code are executed and which aren’t
-# program that asks the user about places they’ve visited
+# Break statement directs the flow of your program; you can use it to control which lines of code are executed and which aren’t
+# Program that asks the user about places they’ve visited
 # We can stop the while loop in this program by calling break as soon as the user enters the 'quit' value:
 prompt = "\nPlease enter the name of a city you have visited: "
 prompt += "\n(Enter 'quit' when you are finished.)" 
