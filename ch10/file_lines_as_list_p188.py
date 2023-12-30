147d959b8880545e32d83ba857d449c1c4f4d193
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 01:29:53 -0500

    Update file_lines_as_list_p188.py
    spelling

--- ch10/file_lines_as_list_p188.py
+++ ch10/file_lines_as_list_p188.py
@@ -1,6 +1,6 @@
 # Making a list of lines from a file
-# when you use with block, the file object returned by open() is only available inside the with block that contains it
-# to retain access to a file's content outside the with block, you store the file's line in a list inside the block and then work with that list
+# When you use with block, the file object returned by open() is only available inside the with block that contains it
+# To retain access to a file's content outside the with block, you store the file's line in a list inside the block and then work with that list
 # program stores the lines of pi_digits.txt in alist inside the with block and then prints the lines outside the with block
 
 filename = 'text_files/pi_digits.txt'
