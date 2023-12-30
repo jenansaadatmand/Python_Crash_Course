4b50b97246b65020047892914bc7e51e5d67702c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:30:17 -0500

    added new file

---
+++ ch06/extensions.py
@@ -0,0 +1,7 @@
+# 6-12 Extensions: We're now working with examples that are complex enough that they can be extended in any number of ways.
+# Use one of the example programs from this chapter,and 
+# Extend it by adding new key and values, changing the context of the program or improving the formating of the output.
+
+
+# Answer:  
+# I have modified a previous saved program 
