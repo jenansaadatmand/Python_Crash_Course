04e644caf89901694abdd498abaa468dccd95cbb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:46:27 -0500

    Update addition.py

--- ch10/addition.py
+++ ch10/addition.py
@@ -1,7 +1,7 @@
 # Addition: one common problem when prompting for numerical input occurs when people provide text instead of numbers
 # When you try to convert the input to an int, you'll get a ValueError
 # Write a program that prompts for two numbers
-# add them together and print the results
+# Add them together and print the results
 # Catch the ValuError if either input value is not a number, 
 # and print a friendly error message
 # Test your program by entering two numbers, and then entering some text instead of a number
