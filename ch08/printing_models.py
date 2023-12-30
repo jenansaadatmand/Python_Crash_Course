d56f4734294b6150097e7750a781ff1b5001c6d3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:35:32 -0500

    added a file

---
+++ ch08/printing_models.py
@@ -0,0 +1,68 @@
+# Modifying a list in a function
+# start with some designs that need to be printed.
+# when passing a list to a function, the function can modify the list contents
+# any changes made within the function body are permanent.
+# a company that creates 3D printed models of designs that user submit
+# consider designs that need to be printed are stored in  alist, and after being printed they're moved to a separate list
+
+# program 1 doing this without a function:
+
+
+unprinted_designs = ['phone case', 'root pendant', 'dodecahedron']
+completed_models = []
+
+# simulate printing each design, until none are left. (use a while loop)
+# move each desing to completed models after printing.
+
+while unprinted_designs:
+    current_design = unprinted_designs.pop() # pop each entry from the end from unprinted_designs, and store them in current_designs
+    print(f"Printing model: {current_design}")
+    completed_models.append(current_design) # adds the design to the completed_model list
+
+# Displays all completed models.
+print("\nThe following models have been printed")
+for completed_model in completed_models: # a for loop, will print each one alone not as a list
+    print(completed_model)
+
+print("\n")
+
+
+# solution 2: restructuring/reorganizing this code using two predefined functions, easier to understand and easier to maintain
+
+# first function will handle printing the design
+# second function will summarize the prints that have been made
+# use descriptive function names for ease of understanding
+
+def print_models(unprinted_designs, completed_models): # define function with two prameters, a list of designs that need to be printed, and a list of completed models
+    """simulate printing each desing, until none are left. 
+       Move each design to completed_models after printing."""
+    while unprinted_designs:
+        current_design = unprinted_designs.pop() # emptying the list of unprinted_designs from end and fills current_designs, then adds/fills current_designs to completed_models 
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+
+def show_completed_models(completed_models): # define a function with one parameter: list of completed models
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed.")
+    for completed_model in completed_models:
+        print(completed_model)   # displays the name of each model that was printed
+
+# this is the body of the program: 
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = [] # empty list that will hold the completed models
+
+# call two functions with the right arguments
+print_models(unprinted_designs, completed_models) # pass two lists when calling the function
+show_completed_models(completed_models) # pass one parameter when calling the function
+
+# if we need to print more designs we simply call print_models() again
+
+# in line 53, replace with line 60, if you don't want to empty the original unprinted_designs list and keep it for your record, then pass a copy of the list when you call it. This can be done using slicing notation [:] from begining to end
+
+# print_models(unprinted_designs[:], completed_models)  # passing a copy using slicing, the original list will not be emptied or affected 
+print(f"\n{unprinted_designs}") # to verify if the original list has been emptied [] if you use line 53 command or if you replace line 53 by line 60, list will be unchanged 
+
+print(completed_models)
+
