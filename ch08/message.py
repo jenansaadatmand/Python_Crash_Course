86ff0d4b55f68382a185c01d756ffce3777587d6
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:30:18 -0500

    added a file

---
+++ ch08/message.py
@@ -0,0 +1,17 @@
+# Message: write a function called disply_message() that prints one sentence telling everyone what you are learning about in this chapter
+# call the function, and make sure the message displays correctly
+
+def disply_message():
+    """Displays a message about what I'am learning."""
+print("We are learning Python functions!")
+disply_message()
+
+print()
+
+#solution # 2: 
+
+def disply_message():
+    """Display a message about what I'm learning."""
+    msg = "I'm learning to store code in functions."
+    print(msg)
+disply_message()
