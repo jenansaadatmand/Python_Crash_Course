923c5c7fdbf7bdc904a8509fb7a012a48abb7051
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:43:44 -0500

    Update imports.py
    edited spelling

--- ch08/imports.py
+++ ch08/imports.py
@@ -1,28 +1,28 @@
-# 8-16 Imports: using a program you wrote that has one function in it,
-# store that function in a separate file
-# import the function into your main program file, 
+# Exercise 8-16 Imports: using a program you wrote that has one function in it,
+# Store that function in a separate file
+# Import the function into your main program file, 
 # and call the function using each of these approcahes:
-# import module_name
-# from module_name import function_name
-# from module_name import function_name as fn
-# import module_name as mn
-# from module_name import *
+# Import module_name
+# From module_name import function_name
+# From module_name import function_name as fn
+# Import module_name as mn
+# From module_name import *
 
 
-# import module_name
+# Import module_name
 
 import build_profile_function
 user_profile = build_profile_function.build_profile('jenan', 'saadatmand', age = 47, hair = 'black', eyes = 'brown', hobby = 'motorbiking')
 print(user_profile)
 print("\n")
 
-# from module_name import function_name
+# From module_name import function_name
 from build_profile_function import build_profile
 user_profile = build_profile('jenan', 'saadatmand', age = 47, hair = 'black', eyes = 'brown', hobby = 'motorbiking')
 print(user_profile)
 print("\n")
 
-# from module_name import function_name as fn
+# From module_name import function_name as fn
 from build_profile_function import build_profile as bp
 user_profile = bp('jenan', 'saadatmand', age = 47, hair = 'black', eyes = 'brown', hobby = 'motorbiking')
 print(user_profile)
