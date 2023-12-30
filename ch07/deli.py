32239fb4b58e8af067f29d305ba240a6b97b79dc
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:08:41 -0500

    Update deli.py
    edited content

--- ch07/deli.py
+++ ch07/deli.py
@@ -1,8 +1,8 @@
 # Deli: make a list called sandwich_orders and fill it with the names of various sandwiches
-# then make an empty list called finished_sandwiches
-# loop through the list of sandwich oders and print a message for each order, such as I made your tuna sandwich
-# as each sandwich is made, move it to the list of finished sandwiches 
-# after all the sandwiches have been made, print a message listing each sandwich that was made.
+# Then make an empty list called finished_sandwiches
+# Loop through the list of sandwich oders and print a message for each order, such as I made your tuna sandwich
+# As each sandwich is made, move it to the list of finished sandwiches 
+# After all the sandwiches have been made, print a message listing each sandwich that was made.
 
 sandwich_orders = ['veggie', 'grilled cheese', 'turkey', 'roast beef']
 finished_sandwiches = []
@@ -14,5 +14,5 @@ while sandwich_orders:
 print("\n")
 for sandwich in finished_sandwiches:
     print(f"I made a {sandwich.title()} sandwich.")
-# extra step to verify what is inside the finished_sandwiches list    
+# Extra step to verify what is inside the finished_sandwiches list    
 print(finished_sandwiches)
