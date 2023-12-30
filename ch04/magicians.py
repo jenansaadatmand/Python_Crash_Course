c9f3408803d5be0bd9f5253db95f631ac04d97d4
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:26:02 -0500

    Update magicians.py

--- ch04/magicians.py
+++ ch04/magicians.py
@@ -6,7 +6,7 @@ magicians = ["alice", "david", "carolina" ] # defining a list
 print(magicians)
 
 for magician in magicians:  # Defining a for loop, pull a name from the list and associate it with a temporary variable magician then title it
-    print(magician.title()) # print the names asscoiated with the variable magician 
+    print(magician.title()) # print the names associated with the variable magician 
     
 # python will repeat the last two lines, iterating through the list
 
