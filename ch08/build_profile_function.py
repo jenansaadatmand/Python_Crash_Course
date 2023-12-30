5e32cb2a577b1bc658312b7de35746ea87647f8e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:28:11 -0500

    added a file

---
+++ ch08/build_profile_function.py
@@ -0,0 +1,9 @@
+# build_profile_function.py
+
+def build_profile(first, last, **user_info): # also can use **kwargs
+    """Builds a dictionary containing everything we know about a user."""
+    user_info['first'] = first
+    user_info['last'] = last
+    return user_info
+
+
