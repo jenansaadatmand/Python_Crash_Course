0617261c32ed31a27f0d1ccbd8e2dc94aa964a33
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:38:24 -0500

    Update intentional_error.py

--- ch03/intentional_error.py
+++ ch03/intentional_error.py
@@ -1,6 +1,6 @@
 # Intentional Error: if you haven't recieved error in one of your programs yet, try to make one happen.  
 # Change an index in one of your programs to produce an index error
-# MAke sure you correct the error before closing the program
+# Make sure you correct the error before closing the program
 
 motorcycles = ['honda', 'suzuki', 'ducati', 'harly']
 print(motorcycles)
