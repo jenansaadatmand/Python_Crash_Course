3d2780a0d55db9a9a9679457d3b3969649e1dbb7
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:08:08 -0500

    Update first_numbers.py

--- ch04/first_numbers.py
+++ ch04/first_numbers.py
@@ -38,4 +38,10 @@ for value in range(1, 11): # loop through the numbers 1, 10 using the range() fu
 print(squares)
 print()
 
+# writing the raising a value to the second power code more conceisly
+squares = []
+for value in range(1, 11):
+    squares.append(value**2)
+print(squares)
+
 
