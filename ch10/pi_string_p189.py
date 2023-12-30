d2087ca2844a66c9bb27f0d03cbd9f56a7885a35
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 01:32:35 -0500

    Update pi_string_p189.py

--- ch10/pi_string_p189.py
+++ ch10/pi_string_p189.py
@@ -1,6 +1,6 @@
 # Large files: One million digits
 # If we start with a text file that contains pi to 1,000,000 decimal places instead of 30 
-# program to create a single string containing all these digits
+# Program to create a single string containing all these digits
 # use the same program as pi_string.py but pass it a different text file that contains the pi to 1,000,000
 # we will just print the fist 50 decimal places
 
