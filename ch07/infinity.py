eb01b9ba9a88462d61f8ddfab55bc134fa0bd7a5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:02:16 -0500

    added a file

---
+++ ch07/infinity.py
@@ -0,0 +1,7 @@
+# Infinity: write a loop that never ends, and run it
+# (To end the loop, press CTRL-C or close the window displaying the output.)
+
+x = 1
+while x <= 10:
+    print(x)
+ #   x += 1   # increment by 1 to get out of the infinite loop
