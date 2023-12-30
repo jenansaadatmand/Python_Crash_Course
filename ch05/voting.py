7a1188927841feb47fedb732db1da183a95447ad
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:17:20 -0500

    added new file

---
+++ ch05/voting.py
@@ -0,0 +1,11 @@
+# Simple if statement code conditional tests whether a person can vote
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote !")
+    print("HAve you registered to vote yet?")
+
+else: 
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+    
