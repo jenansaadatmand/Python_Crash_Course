3ac842af30cccdccbc9d0ebb0920bfe0ee447153
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:44:58 -0500

    Update age_p115.py
    edited spelling

--- ch07/age_p115.py
+++ ch07/age_p115.py
@@ -1,6 +1,6 @@
 # Using int() to Accept Numerical Input
-# when using input() function, python interpret everything the user enters as a string
-# program asks for user's age
+# When using input() function, python interpret everything the user enters as a string
+# Program asks for user's age
 # int() function, treat the input as a numerical value.
 age = input("How old are you? ")
 age = int(age) # converts input string to a numerical value
