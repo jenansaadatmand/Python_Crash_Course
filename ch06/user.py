5f59a7ba5f63e744f0bb98049ecb236f03befe89
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:41:08 -0500

    added new file

---
+++ ch06/user.py
@@ -0,0 +1,26 @@
+# looping through a dictionary designed to store user information on website
+
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+     }
+print(user_0)
+print()
+# for loop to print all value to keys
+for key in user_0:
+    print(user_0[key])
+print()    
+# for loop to peint all keys only
+for i in user_0:
+    print(i)
+# looping and returning all key-value pairs using items() method to iterate over a dictionary
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
+print()
+# alternative looping using item() method and two other variables
+
+for k, v in user_0.items():
+    print(f"\nKey: {k}")
+    print(f"Value: {v}")
