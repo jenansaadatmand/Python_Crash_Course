405f220466aaf34dfa80aac69f8d3f5b59e38592
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:11:41 -0500

    Update no_pastrami.py
    edited spelling

--- ch07/no_pastrami.py
+++ ch07/no_pastrami.py
@@ -1,8 +1,8 @@
 # No pastrami: using the list sandwich_orders from exercise 7-8
-# make sure the sandwich 'pastrami' appears in th e list at least three times
+# Make sure the sandwich 'pastrami' appears in th e list at least three times
 # Add a code near the beginning of your program to print a message saying the deli has run out of pastrami,
 # and then use a while loop to remove all occurences of 'pastrami' from sandwich_orders.
-# make sure no pastrami sandwiches end up in finished_sandwiches
+# Make sure no pastrami sandwiches end up in finished_sandwiches
 
 sandwich_orders = ['pastrami', 'veggie', 'grilled cheese', 'pastrami', 'turkey', 'roast beef', 'pastrami']
 finished_sandwiches = []
