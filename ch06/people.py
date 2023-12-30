d06c9abd861317da834bb56fd66d5070f1550862
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:36:14 -0500

    added new file

---
+++ ch06/people.py
@@ -0,0 +1,35 @@
+# People: Start with the program you wrote for Exercise 6-1 (page 99).
+# Make two new dictionaries representing different people, 
+# and store all three dictionaries in a list called people. 
+# Loop through your list of people.
+# As you loop through the list, 
+# print everything you know about each person.
+
+people = []
+person = {
+    'first_name': 'jimmy', 
+    'last_name': 'saadatmand', 
+    'age': 13, 
+    'city': 'montreal',
+    }
+people.append(person)
+person = {
+    'first_name': 'jenan',
+    'last_name': 'saadatmand',
+    'age': 47,
+    'city': 'montreal',
+    }
+people.append(person)
+person = {
+    'first_name': 'koko',
+    'last_name': 'kiki',
+    'age': 30,
+    'city': 'toronto',
+    }
+people.append(person)
+for person in people:  # looping through the list
+    name = f"{person['first_name'].title()} {person['last_name'].title()}" # storing dictionary key and associated value in variables
+    age = person['age']
+    city = person['city']
+    print(f"{name}, of {city}, is {age} years old.")
+
