27d9cf5a98474fa499b9d318d72c593bce81a448
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:29:48 -0500

    Update large_shirts.py
    edited spelling

--- ch08/large_shirts.py
+++ ch08/large_shirts.py
@@ -1,6 +1,6 @@
 # Large shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love Python.
 # Make a large shirt and a medium shirt with the default message, and 
-# a shirt of any size with a different message
+# A shirt of any size with a different message
 
 def make_shirt(msg='I love Python', size='large'):
     """Prints a message on T-shirts"""
@@ -8,14 +8,14 @@ def make_shirt(msg='I love Python', size='large'):
 make_shirt() # make a large shirt
 make_shirt(size='large'.title())
 print()
-# make a medium shirt
+# Make a medium shirt
 make_shirt(size='medium'.title())
 print()
-# a shirt of any size with a different messge
+# A shirt of any size with a different messge
 make_shirt(size='small'.title(), msg='I love flying')
 print()
 
-# solution # 2: 
+# Solution # 2: 
 def make_shirt(size='large', message='I love Python'):
     """Summarize the shirt that's going to be made"""
     print(f"\nI'm going to make a {size} t-shirt.")
