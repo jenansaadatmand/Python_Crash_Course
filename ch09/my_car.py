2ab4f8bb0cd3bffe10a02fe1d9d4a6c9dca07337
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 00:22:36 -0500

    added a file

---
+++ ch09/my_car.py
@@ -0,0 +1,11 @@
+# Importing class from car module
+
+
+from car import Car   # import classes statement: open car module and import Car class
+
+my_new_car = Car('audi', 'a4', 2019)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
