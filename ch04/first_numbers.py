84a7dd3c6a545dbd956a7098a2aff668d3a4bf39
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:08:32 -0500

    Update first_numbers.py

--- ch04/first_numbers.py
+++ ch04/first_numbers.py
@@ -44,4 +44,14 @@ for value in range(1, 11):
     squares.append(value**2)
 print(squares)
 
+# simple statistics with a list of numbers: 
+# min(), max(), sum()
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+print()
+
 
