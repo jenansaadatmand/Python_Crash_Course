74c70e8e3029d5238256510e261488c6843446a5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:49:19 -0500

    Update printing_models_p155.py
    edited contents

--- ch08/printing_models_p155.py
+++ ch08/printing_models_p155.py
@@ -1,6 +1,6 @@
-# printing models: Put the functions for the example printing_models.py in a separate file called printing_functions.py
-# write an import statement at the top of printing_models_p155.py, 
-#and modify the file to use the imported functions
+# Printing models: Put the functions for the example printing_models.py in a separate file called printing_functions.py
+# Write an import statement at the top of printing_models_p155.py, 
+# and modify the file to use the imported functions
 
 import printing_functions as pf # using as to give a module an alias, shorter name
 
@@ -8,15 +8,15 @@ import printing_functions as pf # using as to give a module an alias, shorter na
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = [] # empty list that will hold the completed models
 
-# call two functions with the right arguments from the imported module
+# Call two functions with the right arguments from the imported module
 
 pf.print_models(unprinted_designs, completed_models) # pass two lists when calling the function
 pf.show_completed_models(completed_models) # pass one parameter when calling the function
 
 
-# in line 53, replace with line 60, if you don't want to empty the original unprinted_designs list and keep it for your record, then pass a copy of the list when you call it. This can be done using slicing notation [:] from begining to end
+# In line 53, replace with line 60, if you don't want to empty the original unprinted_designs list and keep it for your record, then pass a copy of the list when you call it. This can be done using slicing notation [:] from begining to end
 
-# print_models(unprinted_designs[:], completed_models)  # passing a copy using slicing, the original list will not be emptied or affected 
+# Print_models(unprinted_designs[:], completed_models)  # passing a copy using slicing, the original list will not be emptied or affected 
 print(f"\n{unprinted_designs}") # to verify if the original list has been emptied [] if you use line 53 command or if you replace line 53 by line 60, list will be unchanged 
 print(completed_models)
 
