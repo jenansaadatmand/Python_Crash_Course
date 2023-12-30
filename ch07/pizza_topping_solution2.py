d93c368e16d7489c6a85c37accb21fbc8c69a8e8
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:03:06 -0500

    added a file

---
+++ ch07/pizza_topping_solution2.py
@@ -0,0 +1,10 @@
+# 7-4 pizza topping solution 2
+prompt = "\nWhat topping would you like on your pizza?"
+prompt += "\nEnter 'quit' when you are finished:"
+
+while True: 
+    topping = input(prompt)
+    if topping != 'quit':
+        print(f"I'll add {topping} to your pizza.")
+    else: 
+        break
