89a7b7e96cdfbf756d08d32f91cc2416f194cddf
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:36:58 -0500

    Update pass_failing silently.py

--- ch10/pass_failing silently.py
+++ ch10/pass_failing silently.py
@@ -1,4 +1,4 @@
-# failing silently: telling python not report the exception error it catches using a pss statement
+# Failing silently: telling python not report the exception error it catches using a pss statement
 # we will not report to the user the exception we catch
 # let the program fail silently when an exception occurs and continue on as if nothing happened
 # to make the program fail silently, we write the try block as usual, but explicitly tell python to do nothing in the except block using the pass statement.
