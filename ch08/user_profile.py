3068b03f199982e51ed5b977e3f7d7427bc5ce1e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:52:48 -0500

    Update user_profile.py
    edited spelling

--- ch08/user_profile.py
+++ ch08/user_profile.py
@@ -1,12 +1,12 @@
-# using arbitrary keywords arguments
-# sometimes you'll want to accept an arbitrary number of arguments, but you won't know ahead of time what kind of information will be passed to the function
-# in this case, you write functions that accepts as many key-value pairs as the calling statement provides
-# example, building user profiles, you know you will get information about the user profile
-# you know you'll get information about user, but you're not sure what kind of information you'll recieve.
-# the function build_profile() always takes in a first and last name, but it accepts an arbitrary number of keyword arguments as well
-# you'll often see the **kwargs parameter used to collect non-specific keyword arguments
+# Using arbitrary keywords arguments
+# Sometimes you'll want to accept an arbitrary number of arguments, but you won't know ahead of time what kind of information will be passed to the function
+# In this case, you write functions that accepts as many key-value pairs as the calling statement provides
+# Example, building user profiles, you know you will get information about the user profile
+# You know you'll get information about user, but you're not sure what kind of information you'll recieve.
+# The function build_profile() always takes in a first and last name, but it accepts an arbitrary number of keyword arguments as well
+# You'll often see the **kwargs parameter used to collect non-specific keyword arguments
 # The double asterick ** before the user_info parameter causes python to create an empty dictionary called user_info and pack whatever name-value pairs it recieves into this dictionary 
-# within the function, you can access key-value pairs it recives just as you would for any dictionary.
+# Within the function, you can access key-value pairs it recives just as you would for any dictionary.
 
 
 def build_profile(first, last, **user_info): # expects a first and last name, and as many name-value pairs as the user wants to enter, because of ** python stores name-value or key-value pairs in an empty dictionary 
@@ -20,7 +20,7 @@ user_profile = build_profile('albert', 'einstein', location = 'princeton', field
 print(user_profile) # print the variable containing the function call returned data
 print("\n")
 
-# solution 2 using **kwargs (for keyword arguments)
+# Solution 2 using **kwargs (for keyword arguments)
 print("\nSolution when replacing **user_info with **kwargs: \n")
 def build_progile(first, last, **kwargs): # accept as many as keyword arguments = name-value or key-value pairs and store in an empty dictionary)
     """Build a dictionary containing everything we know about a user."""
