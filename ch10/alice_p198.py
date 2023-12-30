dfdb7c8438bc6570c180d3626eb72d621988e813
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:14:12 -0500

    Update alice_p198.py

--- ch10/alice_p198.py
+++ ch10/alice_p198.py
@@ -1,6 +1,6 @@
 # Exception handling helps when you are working with more than one file and one file does not exist
 # Analyzing data or files containing the entire book
-# Pull alice in wonderland book from project Gutenberg
+# Pull alice in Wonderland book from project Gutenberg
 # try to count the number of words in the text
 # use a string method split(), which builds a list of words from a string
 # string() method separates a string into parts wherever it finds space and stores all the parts of a string in a list
