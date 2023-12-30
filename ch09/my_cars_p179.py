6566bedd315eec18789545ac6653db4851b34353
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 00:24:18 -0500

    added a file

---
+++ ch09/my_cars_p179.py
@@ -0,0 +1,10 @@
+# importing a module into a module, when one module depends on a class in another module
+
+from carp178 import Car
+from electric_car_p178 import ElectricCar as EC   # using aliases instead of typing the name of all the class each time, use shorter alias 'EC'
+
+my_beetle = Car('volkswagen', 'beetle', 2019)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = EC('tesla', 'roadster', 2019)
+print(my_tesla.get_descriptive_name())
