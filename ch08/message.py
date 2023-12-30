f0306331175ca5f60fef56e38d8e39784c7234bb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:30:54 -0500

    Update message.py
    edited spelling

--- ch08/message.py
+++ ch08/message.py
@@ -1,5 +1,5 @@
 # Message: write a function called disply_message() that prints one sentence telling everyone what you are learning about in this chapter
-# call the function, and make sure the message displays correctly
+# Call the function, and make sure the message displays correctly
 
 def disply_message():
     """Displays a message about what I'am learning."""
@@ -8,8 +8,8 @@ disply_message()
 
 print()
 
-#solution # 2: 
-
+# Solution # 2: 
+ 
 def disply_message():
     """Display a message about what I'm learning."""
     msg = "I'm learning to store code in functions."
