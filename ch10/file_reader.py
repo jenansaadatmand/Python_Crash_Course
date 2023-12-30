fa3abba70485dc3a25c3d033f28f6d033a63a650
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 00:33:32 -0500

    added a file

---
+++ ch10/file_reader.py
@@ -0,0 +1,18 @@
+# write a program that reads in the contents of a text file and rewrites the file with formatting that allows a browser to display it.
+# work with information in a text file, first step to read the file into memory. read entire file or one line at a time
+# reading an entire file
+# opens a file contains pi to 30 decimal places, with 10 decimal places per line
+# reader program opens the file, reads it, and prints the content of the file to the screen
+# use file relative path or absolute longer path to pass to open() function for the location of the desired file
+with open('text_files/pi_digits.txt') as file_object: # with block is excuted ( to open and close the file properly), open() function to open the file to access its contents, open() function returns an object representing the file pi_digits.txt.
+    contents = file_object.read() # read() method to read entire content of file and store it as one long string in contents variable
+print(contents)
+print(contents.rstrip()) # removing any white space characters from the right side of a string
+
+print("\n")
+
+# reading line by line using a for loop on the file object
+filename = 'text_files/pi_digits.txt' # assign a variable to the file where are reading from
+with open(filename) as file_object:
+    for line in file_object: # use a for loop to loop through the lines in a file
+        print(line.rstrip()) # stripping two blank lines, one invisible newline character at the end of each line from file and one added from print() function recalling
