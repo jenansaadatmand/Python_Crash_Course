f88b951f0b52b16b9513140bec43522d1208256a
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:01:55 -0500

    Update flag_p158.py
    edited spelling

--- ch07/flag_p158.py
+++ ch07/flag_p158.py
@@ -1,14 +1,14 @@
-# using a flag
-# a program in which many different events could cause the program to stop running
+# Using a flag
+# A program in which many different events could cause the program to stop running
 # If many possible events might occur to stop the program, trying to test all these conditions in one while statement becomes complicated and difficult.
 # For a program that should run only as long as many conditions are true, you can define one variable that determines whether or not the entire program is active.
-# this variable is called a flag, acts as a signal to the program 
-# write our programs so they run while the flag is set to True and stop running when any of several events sets the value of the flag to False.
+# This variable is called a flag, acts as a signal to the program 
+# Write our programs so they run while the flag is set to True and stop running when any of several events sets the value of the flag to False.
 # As a result, our overall while statement needs to check only one condition: whether or not the flag is currently True.
 # Then, all our other tests (to see if an event has occurred that should set the flag to False) can be neatly organized in the rest of the program.
 
-# adding a flag to parrot.py
-# flag called active, will monitor wether or not the program should continue running
+# Adding a flag to parrot.py
+# Flag called active, will monitor wether or not the program should continue running
 
 prompt = "\nTell me something, and I will repeat it back to you: "
 prompt += "\nEnter 'quit' to end the program."
