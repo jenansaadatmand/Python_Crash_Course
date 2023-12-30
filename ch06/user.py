4dde802c87facc5a87a4b028a2f2b56ed28681c3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:41:59 -0500

    Update user.py
    edited formats

--- ch06/user.py
+++ ch06/user.py
@@ -1,4 +1,4 @@
-# looping through a dictionary designed to store user information on website
+# Looping through a dictionary designed to store user information on website
 
 user_0 = {
     'username': 'efermi',
@@ -7,19 +7,23 @@ user_0 = {
      }
 print(user_0)
 print()
+
 # for loop to print all value to keys
 for key in user_0:
     print(user_0[key])
 print()    
+
 # for loop to peint all keys only
 for i in user_0:
     print(i)
-# looping and returning all key-value pairs using items() method to iterate over a dictionary
+    
+# Looping and returning all key-value pairs using items() method to iterate over a dictionary
 for key, value in user_0.items():
     print(f"\nKey: {key}")
     print(f"Value: {value}")
 print()
-# alternative looping using item() method and two other variables
+
+# Alternative looping using item() method and two other variables
 
 for k, v in user_0.items():
     print(f"\nKey: {k}")
