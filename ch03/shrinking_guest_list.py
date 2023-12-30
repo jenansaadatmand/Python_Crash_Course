8f18531b6c0a1f99a329be16ee932d9131dbc515
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:46:12 -0500

    Update shrinking_guest_list.py

--- ch03/shrinking_guest_list.py
+++ ch03/shrinking_guest_list.py
@@ -1,7 +1,7 @@
 # Exercise 3-7 shrinking guet list: you just found out that your new dinner table won't arrive in time for diner
 # and now you have space for only two guests
 # Start with your program from exercise 3-6
-# add a new line that printss a ssage saying that you invite only two people for dinner
+# Add a new line that printss a ssage saying that you invite only two people for dinner
 # Use pop() remove guests from your list one at a time until only two names remain in your list
 # Each time you pop a name from your list, print a message to that person 
 # letting them know you're sorry you can't invite them dinner
