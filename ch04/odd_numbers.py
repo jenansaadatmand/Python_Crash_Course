ec0f7741c2fd19b5be3572c2c0399df2557d45b1
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:36:36 -0500

    Update odd_numbers.py
    edited content

--- ch04/odd_numbers.py
+++ ch04/odd_numbers.py
@@ -1,13 +1,13 @@
 # Exercise 4-6 Odd numbers: use the third argument of the range() function to 
 # make a list of the odd numbers from 1 to 20
-# use a for loop to print each number
+# Use a for loop to print each number
 
 odd_nums = list(range(1, 20, 2))
 for odd_num in odd_nums: # print individual numbers one at a time
     print(odd_num)
 print()
 
-# solution 2
+# Solution 2
 
 odd_nums = list(range(1, 20, 2))
 print(odd_nums) # prints in list format
