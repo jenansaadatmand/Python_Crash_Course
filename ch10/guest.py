d9d65028bef26070d11f751331d1451e94534247
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 01:55:29 -0500

    added a file

---
+++ ch10/guest.py
@@ -0,0 +1,15 @@
+# guest: write a program that prompts the user for their name
+# when they respond, write their name to a file called guest.txt
+
+#prompt = 'What is your name? '
+#message = input(prompt)
+#filename = 'text_files/guest.txt'
+#with open(filename, 'a') as file_object:
+#    file_object.write(f"\n{message.title()}")
+
+
+# solution # 2, this program writes the name of the guest and overwrites the previous file
+name = input("What is your name? ")
+filename = 'text_files/guest.txt'
+with open(filename, 'w') as f:
+    f.write(name)    
