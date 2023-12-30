c388411ea16c9350705257d18fd4c84f2663e33b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:21:25 -0500

    added new file

---
+++ ch05/toppings_3.py
@@ -0,0 +1,15 @@
+# using multiple lists and if statements 
+# Watch for unusual toppings requests before building your pizza
+# We make two lists: available topppings and user requested toppings 
+# Available list can be a tupple as well, stable and immutable
+
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:  # loop through the list
+    if requested_topping in available_toppings:  # inside the loop, check to see if each requested topping is in the available list
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+print("\nFinished making your pizza!")    
+
