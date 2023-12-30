583e202fde86ad2777714988a2fa578a9019e614
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:35:53 -0500

    Update guest_list.py

--- ch03/guest_list.py
+++ ch03/guest_list.py
@@ -1,5 +1,5 @@
 # Exercise 3-4: Guest list: If you could invite anyone, living or deceased, to dinner,
-# who would you invite ? Make a list that includes at least three people
+# Who would you invite ? Make a list that includes at least three people
 # you'd like to vite to dinner.   
 # Then use your list to print a message to each person, inviting them to dinner
 
