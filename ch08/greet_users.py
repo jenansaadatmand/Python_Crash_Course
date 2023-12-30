d529aa960f07bfe634d5b18575f22f8d528d2ee4
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:25:33 -0500

    Update greet_users.py
    edited spelling

--- ch08/greet_users.py
+++ ch08/greet_users.py
@@ -1,8 +1,8 @@
-# passing a list to a function
-# list of names, numbers, dictionaries ....etc
-# when you pass a list to a function, the function gets direct access to the contents of the list
-# we have a list of users, we want to print  a greeting to each
-# program sends a personalized greeting to each user by sends a list of names to a function called greet_users(), which greets each person in the list individually
+# Passing a list to a function
+# List of names, numbers, dictionaries ....etc
+# When you pass a list to a function, the function gets direct access to the contents of the list
+# We have a list of users, we want to print  a greeting to each
+# Program sends a personalized greeting to each user by sends a list of names to a function called greet_users(), which greets each person in the list individually
 
 def greet_users(names): # define the function to expect list of names, which it assigns to a parameter names
     """print a simple greeting to each user in the list."""
@@ -14,6 +14,6 @@ greet_users(usernames) # pass the list usernames to the function in our call
 
 print("\n")
 
-# you can call the function anytime using a specific set of users
+# You can call the function anytime using a specific set of users
 usernames_b = ['jimmy', 'jenan', 'tifany']
 greet_users(usernames_b)
