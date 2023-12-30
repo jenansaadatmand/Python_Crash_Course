aa1344d04740545c712385f6b4fc35b48c42f264
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 13:32:10 -0500

    Update names.py

--- ch03/names.py
+++ ch03/names.py
@@ -1,4 +1,4 @@
-# Exercise 3-1 Names: store the names of a few of your friends in a list called names
+# Exercise 3-1 Names: Store the names of a few of your friends in a list called names
 # Print each person's name by accessing each element in the list, one at a time
 
 names=['jimmy', 'mimi', 'tony', 'nana']
