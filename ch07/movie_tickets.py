3840e0c6bca64a363fdf0dad27fe8d6fb4507cbb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:05:47 -0500

    added a file

---
+++ ch07/movie_tickets.py
@@ -0,0 +1,22 @@
+# movie tickets: a movie theater charges different ticket prices depending on a person's age
+# If a person is under the age of 3, the ticker is free
+# if they are between 3 and 12, the ticket is $10
+# and if they are over age 12, the ticket is $15
+# write a loop in which you ask users their age
+# and then tell them the cost of their movie ticket
+
+prompt = "\nHow old are you?"
+prompt += "\nEnter 'quit' after you finished:"
+
+while True:
+    age = input(prompt)
+    if age == 'quit':
+        break
+    age = int(age)
+    
+    if age < 3:
+        print("You get in free!")
+    elif age < 13:
+        print("Your ticket is $10.")
+    else:
+        print("You ticket is $15.")
