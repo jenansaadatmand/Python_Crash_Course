52479fde303e6ba5872506e6b881953e9e0488d5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:31:12 -0500

    added a file

--- ch03/your_own_list.py
+++ ch03/your_own_list.py
@@ -1,11 +1,23 @@
-# Exercise 3-3: Your Own List: Think of your favorite mode of transportation
-# such as a motorcycle or a car, and make a list that stores several examples
-# Use your list to print a series of statements about these items,
-# such as " I would like to own a Honda motorcycle."
+# Think of your favourite mode of transportation, such as a motorcycle or a car,
+# and make a list that stores several examples
+# Use your list to print a series of statements about these items, such as "I would like to own a Honda motorcycle"
 
-transport= ['motorcycle', 'car', 'bus', 'metro', 'train']
-print(f"I would like to own a Honda {transport[0].title()}.")
-print(f"I prefer to go to work using my {transport[1].title()} rather than my {transport[0]}.")
-print(f"Taking the {transport[2].title()} is an environmentally friendly option.")
-print(f"The {transport[3].title()} is close to my house.")
-print(f"Finally, there is always the {transport[-1].title()} as an option.")
+mode_transport = ['motorcycle', 'car', 'bus', 'train', 'aeroplane', 'ship']
+print(f'I would like to own a Honda {mode_transport[0].title()}')
+print(f'I would like to own a Toyota {mode_transport[1].title()}')
+print(f'I would like to own a public {mode_transport[2].title()}')
+print(f'I would like to own a public {mode_transport[3].title()}')
+print(f'I would like to own a flying {mode_transport[4].title()}')
+print(f'I would like to own a crusing {mode_transport[5].title()}')
+print()
+
+
+# solution 2
+mode_transport = ['motorcycle', 'car', 'bus', 'train', 'aeroplane', 'ship']
+msg = 'I would like to own a ' 
+print(f'{msg}{mode_transport[0].title()}')
+print(f'{msg}{mode_transport[1].title()}')
+print(f'{msg}{mode_transport[2].title()}')
+print(f'{msg}{mode_transport[3].title()}')
+print(f'{msg}{mode_transport[4].title()}')
+print(f'{msg}{mode_transport[5].title()}')
