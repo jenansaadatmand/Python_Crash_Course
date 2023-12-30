4f004ae7b97e94b041591cb1304a06db80b1acd3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 20:48:05 -0500

    Update stripping_names.py
    edited content

--- ch02/stripping_names.py
+++ ch02/stripping_names.py
@@ -1,8 +1,8 @@
 # Exercise 2-7: Use a variable to represent a person's name, and include some whitespace
-# characters at the beginning and end of the name.  
+# Characters at the beginning and end of the name.  
 # Make sure you use each character combination, "\t" and "\n", at least once
-# print the name once so the whitespace 
-# then print the name using each of the three stripping functions, 
+# Print the name once so the whitespace 
+# Then print the name using each of the three stripping functions, 
 # lstrip(), rstrip(), strip()
 
 name = "\tjimmy\n\t "
