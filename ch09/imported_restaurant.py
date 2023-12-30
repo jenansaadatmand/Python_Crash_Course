cf9f6dba1a510f8f8b6c68aae26a90bee6a20177
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 00:21:11 -0500

    added a file

---
+++ ch09/imported_restaurant.py
@@ -0,0 +1,7 @@
+# Imported Restaurant: Using your latest Restaurant class
+# Store it in a module
+# Make a separate file that imports Restaurant
+# Make a Restaurant instance, 
+# and call one of Restaurant's methods to show that the import statement is working properly
+
+  
