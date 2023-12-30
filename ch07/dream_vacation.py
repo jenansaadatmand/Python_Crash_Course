b027b6e659d9be7a3cb5c2fd8a333252014f9fa8
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:08:58 -0500

    added a file

---
+++ ch07/dream_vacation.py
@@ -0,0 +1,17 @@
+# Dream vacation: write a program that polls users about their dream vacation
+# write a prompt similar to if you could visit one place in the world, where would you go?
+# include a block of code that prints the results of the poll
+
+responses = {}
+polling_active = True
+while polling_active: 
+    name = input("What is your name?")
+    response = input("If you could visit one place in the world, where would you go?")
+
+    responses[name] = response
+    repeat = input("Would like to let another person respond? ")
+    if repeat == 'no':
+        polling_active = False
+print("--- Polling Results ---")
+for name, response in responses.items():
+    print(f"{name} would like to visit {response} oneday.")
