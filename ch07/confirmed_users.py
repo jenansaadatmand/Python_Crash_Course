99fd1497bfd07bf0a4510f24b237f9af3bd90a77
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:07:43 -0500

    Update confirmed_users.py
    edited spelling

--- ch07/confirmed_users.py
+++ ch07/confirmed_users.py
@@ -1,19 +1,19 @@
-# using a while loop with lists and dictionaries
-# to keep trak of many users and inputs
-# a for loop is useful for looping through a list, but not to modify a list inside a for loop because python has trouble keeping track of items inside a for loop
-# use a while loop, to modify a list or dictionary as you work with it, this allows to collect, store and organize inputs later
-# moving items from one list to another
-# list of newly registered but unverified users of a website
-# after we verify the users, we move them to a separate list of confirmed users by using a while loop to pull users from the list of unconfirmed users as we verify them and then add them to a separate list of confirmed users
+# Using a while loop with lists and dictionaries
+# To keep trak of many users and inputs
+# A for loop is useful for looping through a list, but not to modify a list inside a for loop because python has trouble keeping track of items inside a for loop
+# Use a while loop, to modify a list or dictionary as you work with it, this allows to collect, store and organize inputs later
+# Moving items from one list to another
+# List of newly registered but unverified users of a website
+# After we verify the users, we move them to a separate list of confirmed users by using a while loop to pull users from the list of unconfirmed users as we verify them and then add them to a separate list of confirmed users
 
 
-# start with users that need to be verified,
+# Start with users that need to be verified,
 # and an empty list to hold confirmed users.
 unconfirmed_users = ['alice', 'brian', 'candace']  # begin with a list of unconfirmed users
 confirmed_users = []  # an empty list to hold confirmed users
 
-# verify each user until there are no more unconfirmed users
-# move each verified user into the list of confirmed users
+# Verify each user until there are no more unconfirmed users
+# Move each verified user into the list of confirmed users
 
 while unconfirmed_users:  # while loop runs as long as the list unconfirmed_users is not empty.
     current_users = unconfirmed_users.pop()  # Within this loop, the pop() function removes unverified users one at a time from the end of unconfirmed_users. because Candace is last in the unconfirmed_users list, her name will be the first to be removed, assigned to current_user, and added to the confirmed_users list. Next is Brian,then Alice.
