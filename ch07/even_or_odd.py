cab06f51ed236d3bb9682ad18d4eea6a67d9f2ff
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:49:23 -0500

    added a file

---
+++ ch07/even_or_odd.py
@@ -0,0 +1,13 @@
+# Modulo Operator %, divided one number by another and returns the remainder
+# When one number is divisible by another number, the remainder is 0
+# so the modulo operator always returns 0. 
+# You can use this fact to determine if a number is even or odd:
+# Even numbers are always divisible by two, so if the modulo of a number and two is zero (here, if number % 2 == 0) the number is even. Otherwise, it’s odd.
+# programs guesses the number is even or odd based on % and 0 remainder
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+if number % 2 == 0:
+    print(f"\nThe number {number} is even.")
+else:
+    print(f"\nThe number {number} is odd.")
