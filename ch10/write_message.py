a9c62bc3de9cf77d712acbd37d27fb66890eeb46
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:09:24 -0500

    Update write_message.py

--- ch10/write_message.py
+++ ch10/write_message.py
@@ -6,6 +6,6 @@
 # Python creates the file as it writes in it if it does not exist
 filename = 'text_files/programming.txt'
 with open(filename, 'w') as file_object: # call to open() in this example has two arguments (filename we want to open, write mode)
-    file_object.write("I love programming.\n") # Use write() method on object file to write a string to the file
+    file_object.write("I love programming.\n") # Use write() method on the object file to write a string to the file
     file_object.write("3\n") # numerical values must be converted into strings using str()
     file_object.write("I love creating new games.\n")
