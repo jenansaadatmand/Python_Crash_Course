017ad279ccbc9701e01b04bc8239e5e5bcf4525e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:09:41 -0500

    added a file

---
+++ ch10/guestbook.py
@@ -0,0 +1,17 @@
+# guestbook: write a while loop that prompts users for their name
+# when they enter their name, print a greeting to the screen
+# and add a line recording their visit in a file called guest_book.txt
+# make sure each entry appears on a new line in the file
+
+filename = 'text_files/guest_book.txt'
+print("Please enter 'quit' when you are finished.")
+
+while True:
+    name = input("What is your name? ")
+    if name == 'quit':
+        break
+    else:    
+        with open(filename, 'a') as file_object:
+            file_object.write(f"{name.title()}\n")
+        print(f"Hello {name}, you've been added to the guest book.")
+
