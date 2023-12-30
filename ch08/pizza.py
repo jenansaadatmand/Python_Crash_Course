3b54a7c628a45e6ce92296b303be5b26160e790f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:46:47 -0500

    added a file

---
+++ ch08/pizza.py
@@ -0,0 +1,23 @@
+# Passing an Arbitrary Number of Arguments:
+# in situations where you don't know in advance how many arguments a function needs to accept
+# python allows a function to collect an arbitrary number of arguments from calling statement
+# a function that builds pizza. it needs to accept a number of toppings, but you can't know ahead of time how many topping a person will want
+# the function has one parameter *toppings, but this parameter collects as many arguments as the calling line provides.
+# the asterick * in the parameter tells python to create an empty tupple called toppings and pack whatever values it recieves into this tupple
+# the printed output is a tupple even if the function recieves one argument only eg:('pepperoni',)
+
+#def make_pizza(*toppings):  # the * asterick allows the function to accept arbitrary number of parameters that allow passing an arbitrary number of arguments 
+#    """Print the list of toppings that have been requested."""
+#    print(toppings)
+#make_pizza('pepperoni') # only one value in the argument
+#make_pizza('mushrooms', 'green peppers', 'extra cheese') # passing arbitrary number of arguments or values in the calling of the function
+
+# Now we can replace the print() call with a loop that runs through the list of toppings and describes the pizza being ordered
+
+def make_pizza(*toppings):
+    """Summarize the pizza we are about to make."""
+    print("\nMaking pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
