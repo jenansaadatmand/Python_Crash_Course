04821465e9df9c95bf23c5a0aa6ed06b1c8d91df
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:06:18 -0500

    added new file

---
+++ ch05/alien_colors_2.py
@@ -0,0 +1,21 @@
+# Alien colors # 2: Choose a color for an alien as you did in exercise 5-3 and write an if-else chain
+# If the alien's color is green, print a statement that the player just earned 5 points for shooting the alien
+# If the alien'd color isn't green, print a statement that the player just earned 10 points
+# Write one version of this program that runs the if block and another that runs the else block
+
+
+# Version if block runs:
+
+alien_colors = 'green'
+if alien_colors == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+print()
+
+# Version else block runs :
+alien_colors = 'yellow'
+if alien_colors == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
