9f8a304b41936aebe43a3df86c58df0d0f0d9c35
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:32:16 -0500

    Update favourite_places.py
    edited content

--- ch06/favourite_places.py
+++ ch06/favourite_places.py
@@ -1,9 +1,9 @@
 # Favourite place: Make a dictionary called favourite_places
 # Think of three names to use as keys in the dictionary
-# and store one to three favourite places for each person
+# and store one to three favorite places for each person
 # To make this exercise a bit more interesting, 
-# ask some friends to name a few of their favourite places
-# loop through the dictionary, and print each person's name and their favourite places
+# Ask some friends to name a few of their favorite places
+# Loop through the dictionary, and print each person's name and their favorite places
 
 
 favourite_places = {
