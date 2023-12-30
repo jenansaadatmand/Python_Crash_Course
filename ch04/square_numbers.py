634d402eeb7d39c1e03492cedf325df06f0d38c3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:43:45 -0500

    Update square_numbers.py
    edited content

--- ch04/square_numbers.py
+++ ch04/square_numbers.py
@@ -1,4 +1,4 @@
-# square numbers, make a list of the first 10 square numbers 
+# Square numbers, make a list of the first 10 square numbers 
 squares = []
 for value in range(1, 11):
     square = value**2 # assigning to a variable
@@ -6,21 +6,21 @@ for value in range(1, 11):
 print(squares)
 print()
 
-# solution 2: 
+# Solution 2: 
 squares = []
 for value in range(1, 11):
     squares.append(value**2) # appending directly to the list no variable assignement 
 print(squares)
 print()
 
-# solution 3
+# Solution 3
 squares = []
 for x in range(1, 11):
     squares.append(x**2) # appending to the list direclty, no assignment to a variable 
 print(squares)
 print()
 
-# list comprehension 
+# List comprehension 
 squares = [value**2 for value in range(1, 11)]
 print(squares)
 
