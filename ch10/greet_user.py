093b93e47e31299b66c04c17b3d9eba49eee4242
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 03:06:29 -0500

    Update greet_user.py

--- ch10/greet_user.py
+++ ch10/greet_user.py
@@ -4,5 +4,5 @@ import json
 filename = 'username.json'
 with open(filename) as f:
     greetings = json.load(f) # to read the stored information in username.json, assign to variable
-    print(f"Welcome back, {greetings.title()}!") # print recovered username, we welcome back the user once logged in again
+    print(f"Welcome back, {greetings.title()}!") # Print recovered username, we welcome back the user once logged in again
     
