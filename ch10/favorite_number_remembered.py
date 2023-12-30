6e707d26bd2302673f21e17ce2ad5a4a694bb06d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 03:18:55 -0500

    Update favorite_number_remembered.py

--- ch10/favorite_number_remembered.py
+++ ch10/favorite_number_remembered.py
@@ -1,6 +1,6 @@
 # Favorite number remembered: combine the two programs from execise 10-11 into one file.
 # If the number is already stored, report the favorite number to t he user.
-# if not, prompt for the user's favorite number and store in a file
+# If not, prompt for the user's favorite number and store in a file
 # Run the program twice to see that it works.
 
 # program using try-except-else block in a function
