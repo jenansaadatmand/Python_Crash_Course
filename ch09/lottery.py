0146887e99c0aeb0abf6e096963950b3bc547024
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 00:31:08 -0500

    Update lottery.py
    edited spelling

--- ch09/lottery.py
+++ ch09/lottery.py
@@ -1,6 +1,6 @@
-# lottery: make a list or tuple containing a series of 10 numbers and five letters
+# Lottery: make a list or tuple containing a series of 10 numbers and five letters
 # Randomly select four numbers or letters from the list and 
-# print a message saying that any ticket matching these four numbers or letters wins a prize
+# Print a message saying that any ticket matching these four numbers or letters wins a prize
 
 
 from random import randint, choice
@@ -18,7 +18,7 @@ print(f"Any ticket matching these four numbers or letters wins a prize!: '{winni
 
 print()
 
-# solution 2
+# Solution 2
 
 possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
 
