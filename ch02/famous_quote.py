6a607ec44cf154a237d53f9c020d5f1ba58863cc
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:07:33 -0500

    Update famous_quote.py
    edited code

--- ch02/famous_quote.py
+++ ch02/famous_quote.py
@@ -1,8 +1,14 @@
 # Exercise 2-5: Find a quote from a famous person you admire.
 # print the quote and the name of its author
-# your ouput should look somthing like the following, including the quotation marks:
+# Your output should look something like the following, including the quotation marks:
 # Alber Einstein once said, "A person who never made a mistake never tried anything new." 
 
+# Solution 1:
 quote = "The greatest glory in living lies not in never falling, but in rising every time we fall."
 author = "albert einstein"
 print(f'{author.title()} once said, "{quote}"')
+
+# Solution 2:
+quote = '"If you can’t explain it simply, you don’t understand it well enough."'
+print(f"Albert Einstein once said, {quote}")
+
