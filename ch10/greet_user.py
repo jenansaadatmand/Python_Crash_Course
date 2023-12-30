935b2a6467887a320ca45d926596b87acf168e44
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 03:05:46 -0500

    added a file

---
+++ ch10/greet_user.py
@@ -0,0 +1,8 @@
+# program that greets a user whose name has already been stored
+
+import json
+filename = 'username.json'
+with open(filename) as f:
+    greetings = json.load(f) # to read the stored information in username.json, assign to variable
+    print(f"Welcome back, {greetings.title()}!") # print recovered username, we welcome back the user once logged in again
+    
