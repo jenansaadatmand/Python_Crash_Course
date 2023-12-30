c5c626266f1fe345624e666823fb789eed780d5d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:53:14 -0500

    added a file

---
+++ ch07/rental_car.py
@@ -0,0 +1,5 @@
+# Rental car: write a program that asks the user what kind of rental car they would like.
+# print a message about that car, such as "let me see if I can find you a Subaru." 
+
+car = input("What kind of rental car you would like? ")
+print(f"\nLet me see if I can find you a {car.title()}.")
