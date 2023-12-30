0ddfa34bf4ea20b7de13173a1c2110cc2f290e4c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:44:44 -0500

    Update make_pizza_p148.py
    edited contents

--- ch08/make_pizza_p148.py
+++ ch08/make_pizza_p148.py
@@ -1,8 +1,8 @@
-# mixing positional and arbitrary arguments
-# if you want a function to accept several different kinds of arguments, the parameter that accepts the arbitrary arguments must be placed last in the function definition.
-# python matches positional and keyword arguments first and then collects any remaining arguments in the final parameter
-# if the function needs to take in a size for the pizza, that parameter must  come before the parameter *toppings:
-# you'll often see the generic parameter name *args, which collects arbitrary positional arguments like this
+# Mixing positional and arbitrary arguments
+# If you want a function to accept several different kinds of arguments, the parameter that accepts the arbitrary arguments must be placed last in the function definition.
+# Python matches positional and keyword arguments first and then collects any remaining arguments in the final parameter
+# If the function needs to take in a size for the pizza, that parameter must  come before the parameter *toppings:
+# You'll often see the generic parameter name *args, which collects arbitrary positional arguments like this
 
 
 #def make_pizza(size, *toppings): # python assigns the first value it recieves to the parameter size, all other values that come after are stored in toppings tupple
@@ -15,7 +15,7 @@
 #make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese') # the function call include an argument for the size first, followed by as many as toppings as needed
 
 
-# solution 2 using *args
+# Solution 2 using *args
 
 def make_pizza(size, *args): # * args for arbitrary arguments to be saved in a tupple
     """Summarize the pizza we are about to make."""
