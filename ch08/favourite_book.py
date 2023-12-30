3070a383f13a21e4a0dc273e4425e95a58be2776
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:22:26 -0500

    added a file

---
+++ ch08/favourite_book.py
@@ -0,0 +1,10 @@
+# Favourite Book: write a function called favourite_book() that accepts one parameter, title
+# The function should print a message, such as One of my favourite books is Alice in Wobderland
+# call the function, making sure to include a book title as argument in the function call
+
+def favourite_book(title):
+    """Display a message about someone's favourite book"""
+    print(f"{title} is one of my favourite books.")
+favourite_book('alice in wonderland')
+print()
+favourite_book('the abstract wild')
