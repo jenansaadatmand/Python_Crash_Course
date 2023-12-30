c257cff613f85370ac95f81ac6cc8a1522daa95c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 00:23:26 -0500

    added a file

---
+++ ch09/my_cars_entire_module.py
@@ -0,0 +1,16 @@
+# You can also import an entire module and then access the classes you need using dot notation.
+# access classes through module_name.ClassName syntax
+# also you can import every class from a module using the following syntax:
+# from module_name import * 
+# this syntax not recommended, not clear which class you are using from the module when you read it and there is a chance of overwriting exisiting or confuse names of Class and functions in your program 
+
+import car   # import the entire Module and use dot notation to access classes
+
+my_beetle = car.Car('volkswagen', 'beetle', 2019)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = car.ElectricCar('tesla', 'roadster', 2019)
+print(my_tesla.get_descriptive_name())
+
+
+
