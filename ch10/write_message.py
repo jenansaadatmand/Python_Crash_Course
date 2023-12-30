02451127cf7197c05996add0f0c5b8d87265f4ea
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:08:51 -0500

    Update write_message.py

--- ch10/write_message.py
+++ ch10/write_message.py
@@ -2,7 +2,7 @@
 # To write text to a file, you need to call open() with a second argument telling Python that you want to write to the file.
 # Program writes a simple message and stores it in a file instead of printing it to screen
 # You can open a file in 'r' read, 'w' write, 'a' append, 'r+' read and write modes
-# Be be careful opening a file in write mode ('w') because if the file does exist, Python will erase the contents of the file before returning the file object.
+# Be careful opening a file in write mode ('w') because if the file does exist, Python will erase the contents of the file before returning the file object.
 # Python creates the file as it writes in it if it does not exist
 filename = 'text_files/programming.txt'
 with open(filename, 'w') as file_object: # call to open() in this example has two arguments (filename we want to open, write mode)
