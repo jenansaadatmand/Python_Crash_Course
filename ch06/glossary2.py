e2e28ae1b8fde6d6a47468d3fad6bd42a464a684
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:35:04 -0500

    added new file

---
+++ ch06/glossary2.py
@@ -0,0 +1,35 @@
+# Glossar 2: Now that you know how to loop through a dictionary
+# clean up the code from excersize 6-3 (page 99) by replacing your series of print() calls with a loop that runs through the dictionary's keys and values
+# when you're sure that your loop works, add five more python terms to your glossary
+# when you run your program again, these new words and meanings should automatically be included in the output
+
+glossary = {
+    'variable': 'store data',
+    'list': 'collection of items in particula order',
+    'dictionary': 'collection of key-value pair',
+    'constant': 'All capital letters variable whose value stays the same throughout the life of the program',
+    'conditional test': 'expression within if statment that can be evaluated as True or False',
+    }
+for word, definition in glossary.items(): 
+    print(f"\nWord: {word}")
+    print(f"Definition: {definition}")   
+print()
+
+# solution 2
+for word, definition in glossary.items():
+    print(f"\n{word.title()}; {definition} ")
+print()
+
+# Adding 5 new words and meanings
+glossary['floating point'] = 'numerical value with a decimal point' 
+glossary['key'] = 'The first item in a key-value pair in a dictionary'   
+glossary['string'] = ' a series of characters'
+glossary['loop'] = 'work through a collection of items, one at a time'
+glossary['boolean expression'] = 'An expression that evaluates to True or False'
+for word, definition in glossary.items():
+    print(f"\nWord: {word}")
+    print(f"Definition: {definition}")
+print()
+print()
+for word, definition in glossary.items():
+    print(f"\n{word.title()}: {definition}")
