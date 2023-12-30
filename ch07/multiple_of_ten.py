9a9f49eeca87eb60c53aa4c5821845727d8fc787
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:55:56 -0500

    added a file

---
+++ ch07/multiple_of_ten.py
@@ -0,0 +1,18 @@
+# Multiples of ten:
+# Ask the user for a number, and then report wether the number is a multiple of 10 or not
+
+number = input("Please provide a number?")
+number = int(number)
+if number % 10 == 0:
+    print(f"The number provided {number} is a multiple of ten")
+else:
+    print(f"You're number provided {number} is not a multiple of ten")
+
+# solution 2 
+number = input("Give me a number, please: ")
+number = int(number)
+
+if number % 10 == 0:
+    print(f"{number} is a multiple of 10.")
+else:
+    print(f"{number} is not a multiple of 10.")
