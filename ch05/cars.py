d894ee14d01bf6184ee300a03f97044bc7d0addb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:04:20 -0500

    Update cars.py
    edited content

--- ch05/cars.py
+++ ch05/cars.py
@@ -1,16 +1,16 @@
-# Sorting a list alphabetically and permenantly using sort() method
+# Sorting a list alphabetically and permanently using sort() method
 cars=["bmw", 'audi', 'toyota', 'saburu']
 cars.sort()
 print(cars)
-# sorting the list in reverse-alphabetical order by passing the argument reverse=True in sort() method
+# Sorting the list in reverse-alphabetical order by passing the argument reverse=True in sort() method
 # using sort(reverse=True)
 
 cars.sort(reverse=True)
 print(cars)
 print()
 
-# sorting a list temporarily and without affecting the original order
-# use sorted() funtion
+# Sorting a list temporarily and without affecting the original order
+# use sorted() function
 
 cars = ['bmw','audi', 'toyota', 'saburu']
 print("Here is the original list:")
@@ -20,24 +20,24 @@ print(sorted(cars))
 print("\nHere is the original list agian:") # unchanged original order of list
 print(cars)
 
-# sorted() function can also accept reverse=True argument
+# Sorted() function can also accept reverse=True argument
 print("\nHere is the sorted list in reverse-order:")
 print(sorted(cars, reverse=True))
 print(cars) # list original order is unchanged
 print()
 
 # Printing a list original order in reverse order using reverse() method 
-# permenantly reversing the original chronological order of the list
+# Permenantly reversing the original chronological order of the list
 cars = ['bmw', 'audi', 'toyota', 'saburu']
 print("Here is the original list chronological order in reverse:")
 cars.reverse()
 print(cars)
 print()
 
-# to revert back to original list order apply reverse() method a second time
+# To revert back to original list order apply reverse() method a second time
 print("reverted list to original order after second reverse():")
 cars.reverse()
 print(cars)
 
-# finding the length of a list
+# Finding the length of a list
 print(len(cars))
