8bbfbbbc39c0e46f8f3e1f31a5cf79d22cf2bd9d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 02:59:23 -0500

    Update number_writer.py

--- ch10/number_writer.py
+++ ch10/number_writer.py
@@ -1,5 +1,5 @@
 # Storing Data/information into data structures (lists and dictionaries). eg: from user input, user preferencesin game, profivde data for visualizaton
-# use JASON module (Javascript object notation format).
+# Use JASON module (Javascript object notation format).
 # when user close a program, he saves the information entered 
 # json module allows you to dump simple python data structures into a file and load the data from that file next time the program runs
 # json module can be used to share data between different python programs
