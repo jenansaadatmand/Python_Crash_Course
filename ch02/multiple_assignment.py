b4880d3e64f7ef1eb7591989169efb702c079604
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:10:19 -0500

    added a file

---
+++ ch02/multiple_assignment.py
@@ -0,0 +1,19 @@
+# Multiple assignment. assign values to more than one variabble using just a single line
+# Technique useful for when initializing a set of numbers
+# How you can initialize the variables x, y, z to zero
+
+x, y, z = 0, 0, 0
+print(x, y, z)
+print(x)
+print(y)
+print(z)
+
+# Underscore in large numbers.  group 3 digitd togeather
+universe_age = 14_000_000_000
+print(universe_age)
+
+# Constants. is like a variable whose value stays the same throughout the life of a program. users cannot change it
+# python uses all capital letters to indicate a variable should be treated as a constant and never be changed
+
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
