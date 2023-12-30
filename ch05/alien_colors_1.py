4920d293636dc1cfbc09bb3c5d5e9f29faf3e156
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 22:02:40 -0500

    added new file

---
+++ ch05/alien_colors_1.py
@@ -0,0 +1,17 @@
+# Alien colors # 1: imagine an alien was just hot down in a game
+# create a variable called alien_color and 
+# assign it a value of 'green', 'yellow', or 'red'
+# write an if statement to test whether the alien's color is green
+# If it is, print a message that the player just earned 5 points
+# write one version of this program that passes the if test and another that fails
+# ( the version that fails will have no output.)
+
+# Passing version
+alien_color = 'green' 
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+# Failing version that fails the test will have no output
+alien_color = 'red'
+if alien_color == 'green':
+    print("You just earned 5 points!")
