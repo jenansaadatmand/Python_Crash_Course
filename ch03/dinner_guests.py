0111c8c52c9ba91654d537c821ce81320bd17c9b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:19:13 -0500

    Update dinner_guests.py
    edited code

--- ch03/dinner_guests.py
+++ ch03/dinner_guests.py
@@ -1,3 +1,16 @@
 # Exercise 3-9 dinner guests: working with one of the programs from exercises 3-4 through 3-7 (pages 41-42)
 # use len() to print a message indicating the number of people you're inviting to dinner.
-# I choose exercises 3-7 shrinking guest list and 3-6 more guests  
+# I choose exercises 3-4 
+
+# Exercise 3-4 guest_list.py
+guests = ['jenan saad', 'lili sad', 'toto saa']
+print(f'{guests[0].title()}, you are invited to dinner!')
+print(f'{guests[1].title()}, you are invited to dinner!')
+print(f'{guests[2].title()}, you are invited to dinner!')
+print() 
+# solution 1:
+num_guests = len(guests)
+print(f'\nThe number of guests invited to dinner: {num_guests}')
+
+# solution 2:
+print(f'\nThe number of guests invited to dinner: {len(guests)}')
