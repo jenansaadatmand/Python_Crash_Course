127b20a55437a42227311dbcce09b40f6f027c9c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 00:27:37 -0500

    Update random_p180.py
    edited spelling

--- ch09/random_p180.py
+++ ch09/random_p180.py
@@ -1,7 +1,7 @@
-# random module contains the randint() 
+# Random module contains the randint() 
 #ranint() takes two integer arguments and return a randomly selected integer between (and including) those numbers.
 
-# how to generate a random number between 1 and 6:
+# How to generate a random number between 1 and 6:
 
 from random import randint  # ranint() function takes in a two integer numbers and returns a number in betwen (and including) the those numbers
 print(randint(1,6))
@@ -15,5 +15,5 @@ first_up = choice(players)  # choice() take in list players
 print(first_up)
 
 
-# you can alternatively import both functions from the module using this syntax
+# You can alternatively import both functions from the module using this syntax
 # from random import randit, choice
