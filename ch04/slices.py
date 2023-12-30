28013c0711e15ca68f43febe1ac35834217bf575
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 21:56:52 -0500

    added new file

---
+++ ch04/slices.py
@@ -0,0 +1,20 @@
+# Slices: using one of the programs you wrote in this chapter
+# Add several lines to the end of the program that do the following:
+# Print the message The first three items in the list are: 
+# Then use a slice to print the first three items from that program's list
+# Print the message Three items from the middle of the list are: 
+# Use a slice to print three items from the middle of the list
+# Print the message The last three items in the list are:
+
+
+
+# Use a slice to print the last three items in the list
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print("The first three items in the list are:")
+print(players[0:3])
+print("Three items from the middle of the list are:")
+print(players[1:4])
+print("The last three items in the list are:")
+print(players[-3:])
+print(players[2:])
