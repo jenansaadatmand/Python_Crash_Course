b85ffbaef8739434f63dcc10284ea682c7892baf
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-30 00:25:42 -0500

    Update my_cars.py
    edited spelling

--- ch09/my_cars.py
+++ ch09/my_cars.py
@@ -1,7 +1,7 @@
-# importing multiple Classes from a Module
-# if we want to make a regular car and an electric car (two instances) in the same file, import both classes Car and ElectricCar
+# Importing multiple Classes from a Module
+# If we want to make a regular car and an electric car (two instances) in the same file, import both classes Car and ElectricCar
 
-from car import Car, ElectricCar  # separate each class with a commma
+from car import Car, ElectricCar  # separate each class with a comma
 
 my_beetle = Car('volkswagen', 'beetle', 2019)
 print(my_beetle.get_descriptive_name())
