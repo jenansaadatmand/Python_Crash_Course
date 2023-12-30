a5198a4e961395c813ed2597e0e14a6aa52095b5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-29 23:22:01 -0500

    Update cities.py
    edited content

--- ch08/cities.py
+++ ch08/cities.py
@@ -1,6 +1,6 @@
 # cities: write a function called describe_city() that accepts the name of a city and its country
 # The function should print a simple sentence, 
-# such as Reykjavik is in Iceland.
+# Such as Reykjavik is in Iceland.
 # Give the parameter for the country a default value
 # Call your function for three different cities, at least one of which is not in the default country
 
@@ -14,7 +14,7 @@ describe_city('toronto', 'Canada')
 
 print()
 
-# solution # 2:
+# Solution # 2:
 
 def describe_city(city, country='chile'):
     """Describe a city."""
