e04c626ce7887faef93857e8731628a9b1cecb25
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:24:09 -0500

    Update rental_car.py

--- ch07/rental_car.py
+++ ch07/rental_car.py
@@ -1,5 +1,5 @@
 # Rental car: write a program that asks the user what kind of rental car they would like.
-# Print a message about that car, such as "let me see if I can find you a Subaru." 
+# Print a message about that car, such as "Let me see if I can find you a Subaru." 
 
 car = input("What kind of rental car you would like? ")
 print(f"\nLet me see if I can find you a {car.title()}.")
