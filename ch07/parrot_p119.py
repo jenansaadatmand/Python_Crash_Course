d6c4341736b82e612156a6f441c1055c1b4607a3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:20:53 -0500

    Update parrot_p119.py

--- ch07/parrot_p119.py
+++ ch07/parrot_p119.py
@@ -15,6 +15,6 @@ while message != 'quit':  # compare the value of the message to 'quit',
 # The first time the program runs and Python reaches the while statement, it needs to compare the value of message to 'quit', but no user input has been entered yet. If Python has nothing to compare, it won’t be able to continue running the program. 
 # To solve this problem, we make sure to give the message an initial value. Although it’s just an empty string, it will make sense to Python and allow it to perform the comparison that makes the while loop work.
 # While loop w runs as long as the value of the message is not 'quit'.
-# The first time through the loop, message is just an empty string, so Python enters the loop. At message = input(prompt), Python displays the prompt and waits for the user to enter their input. Whatever they enter is assigned to message and printed; then, 
+# The first time through the loop, the message is just an empty string, so Python enters the loop. At message = input(prompt), Python displays the prompt and waits for the user to enter their input. Whatever they enter is assigned to message and printed; then, 
 # Python reevaluates the condition in the while statement. As long as the user has not entered the word 'quit', the prompt is displayed again and Python waits for more input. 
 # When the user finally enters 'quit', Python stops executing the while loop and the program ends
