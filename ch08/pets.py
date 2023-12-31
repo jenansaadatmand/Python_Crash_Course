993755766454e80392294c8a1336ee35d00d88ad
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:55:38 -0500

    Update pets.py

--- ch08/pets.py
+++ ch08/pets.py
@@ -4,7 +4,7 @@ def describe_pet(animal_type, pet_name): # Parameters included
     """Display information about a pet."""
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
-describe_pet('hamster', 'harry')  # pass postional argument when recalling a function
+describe_pet('hamster', 'harry')  # Pass postional argument when recalling a function
 
 
 # Multiple function calls, calling a function multiple times 
