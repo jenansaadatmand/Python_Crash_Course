d6785bc5f6f1c3be047dbf7c3c4df686b7532f23
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 18:24:01 -0500

    Update user_profile_p150.py

--- ch08/user_profile_p150.py
+++ ch08/user_profile_p150.py
@@ -15,7 +15,7 @@ print("\n")
 
 
 # Solution 2:
-def build_profile(first, last, **user_info): # also can use **kwargs
+def build_profile(first, last, **user_info): # Also can use **kwargs
     """Builds a dictionary containing everything we know about a user."""
     user_info['first'] = first
     user_info['last'] = last
