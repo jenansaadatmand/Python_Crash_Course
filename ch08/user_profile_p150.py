6e03df1b0467daa2529207190d4f86836f26ffcb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 18:23:08 -0500

    Update user_profile_p150.py

--- ch08/user_profile_p150.py
+++ ch08/user_profile_p150.py
@@ -3,7 +3,7 @@
 # Using your first and last names and three other key-value pairs that describe you
 
 
-def build_profile(first, last, **user_info): # also can use **kwargs
+def build_profile(first, last, **user_info): # Also can use **kwargs
     """Builds a dictionary containing everything we know about a user."""
     user_info['first'] = first
     user_info['last'] = last
