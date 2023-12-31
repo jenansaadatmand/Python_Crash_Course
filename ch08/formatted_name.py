70379a7f80bce940abd28884372fcf2b203b3d16
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:37:06 -0500

    Update formatted_name.py

--- ch08/formatted_name.py
+++ ch08/formatted_name.py
@@ -25,7 +25,7 @@ print()
 # Expand get_formatted_name() to handle middle names as well
 # This function always requires a middle name argument
 def get_formatted_name(first_name, middle_name, last_name):
-        """Return a full name, nestly formatted"""
+        """Return a full name, neatly formatted"""
         full_name= f"{first_name} {middle_name} {last_name}"
         return full_name.title()
 musician = get_formatted_name('john','lee','hooker')
