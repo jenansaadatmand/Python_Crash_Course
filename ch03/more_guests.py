4d9bdb07a3abbe19c30ac586405fa84f0ced21e1
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:47:45 -0500

    Update more_guests.py

--- ch03/more_guests.py
+++ ch03/more_guests.py
@@ -2,7 +2,7 @@
 # More space is available. Think of three guests to invite to dinner
 # Start with your program from exercise 3-4 or 3-5
 # Add a print() call to the end of your program, informing people that you found a bigger table
-# use insert() to add one new guest to the beginning of your list
+# Use insert() to add one new guest to the beginning of your list
 # use insert() to add one new guest to the middle of your list
 # use append() to add one new guest to the end of your list
 # Print a new set of invitation messages, one for each person in your list
