f9107916b71caab9ba33985662760c7419462d40
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:54:52 -0500

    Update person.py

--- ch08/person.py
+++ ch08/person.py
@@ -6,7 +6,7 @@ def build_person(first_name, last_name): # Function takes in first and last name
     person = {'first': first_name, 'last': last_name} # A dictionary is created, value ot first_name stored in first key, value of last_name stored in key last
     return person # entire dictionary returned
 musician = build_person('jimi', 'hendrix')
-print(musician) # prints a dictionary with key and values
+print(musician) # Prints a dictionary with key and values
 print()
 
 
