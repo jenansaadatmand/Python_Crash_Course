7d6dfe5c30c5b3c93440021f7c54acca8da3ac14
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 08:52:52 -0500

    Update alien_colors_1.py

--- ch05/alien_colors_1.py
+++ ch05/alien_colors_1.py
@@ -4,7 +4,7 @@
 # Write an if statement to test whether the alien's color is green
 # If it is, print a message that the player just earned 5 points
 # Write one version of this program that passes the if test and another that fails
-# (the version that fails will have no output.)
+# (The version that fails will have no output.)
 
 # Passing version
 alien_color = 'green' 
