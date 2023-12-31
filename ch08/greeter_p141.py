570d25105ad949383d023e1ea3b7ed8d21e4ba83
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:47:46 -0500

    Update greeter_p141.py

--- ch08/greeter_p141.py
+++ ch08/greeter_p141.py
@@ -5,7 +5,7 @@ def get_formatted_name(first_name, last_name):
     """Return a full name, neatly formatted."""
     full_name = f"{first_name} {last_name}"
     return full_name.title()
-# This is an infinite loop !
+# This is an infinite loop!
 while True:  # while loop asks the user to enter their name, and we prompt for their first and last name separately
     print("\nPlease tell me your name:")
     print("(enter 'q' at any time to quit)")
