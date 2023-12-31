53bef39b17d84e2a9342e6d3b0b42296322f9edc
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:08:32 -0500

    Update counting_p122.py

--- ch07/counting_p122.py
+++ ch07/counting_p122.py
@@ -7,4 +7,4 @@ while current_number < 10: # python enters the loop
     current_number += 1  # Increment the count by 1, so the current number is 1
     if current_number % 2 == 0: # if statement checks the modulo of current_number and 2, if modulo is 0 (current_number is divisible by 2)
         continue   # continue statement to ignore the rest of the loop and return to the beginning
-    print(current_number)  # if current_number is not divisible by 2, the rest of the loop is excuteed and prints the current_number
+    print(current_number)  # If current_number is not divisible by 2, the rest of the loop is excuteed and prints the current_number
