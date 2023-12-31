2eca80055418629ed260b3c83bfc3de4db7ff0e8
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:48:05 -0500

    Update more_guests.py

--- ch03/more_guests.py
+++ ch03/more_guests.py
@@ -4,7 +4,7 @@
 # Add a print() call to the end of your program, informing people that you found a bigger table
 # Use insert() to add one new guest to the beginning of your list
 # Use insert() to add one new guest to the middle of your list
-# use append() to add one new guest to the end of your list
+# Use append() to add one new guest to the end of your list
 # Print a new set of invitation messages, one for each person in your list
 
 guest_list = ['jimmy','tiffany', 'spoiler']
