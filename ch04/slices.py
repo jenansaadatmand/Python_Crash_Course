69eff98c741599b0b30f77965a1f94913c12045d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 08:49:18 -0500

    Update slices.py

--- ch04/slices.py
+++ ch04/slices.py
@@ -1,6 +1,6 @@
 # Slices: using one of the programs you wrote in this chapter
 # Add several lines to the end of the program that do the following:
-# Print the message The first three items in the list are: 
+# Print the message the first three items in the list are: 
 # Then use a slice to print the first three items from that program's list
 # Print the message Three items from the middle of the list are: 
 # Use a slice to print three items from the middle of the list
