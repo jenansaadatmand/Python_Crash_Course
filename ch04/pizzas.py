74b34549571908c8f7968ebe54edeea12f4c31be
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 08:38:00 -0500

    Update pizzas.py

--- ch04/pizzas.py
+++ ch04/pizzas.py
@@ -2,7 +2,7 @@
 # Store these pizza names in a list, and 
 # Then use a for loop to print the name of each pizza
 # Modify your for loop to print a sentence using the name of the pizza,
-# instead of printing just the name of the pizza.
+# Instead of printing just the name of the pizza.
 # For each pizza, you should have one line of output 
 # containing a simple statement like I like pepperoni pizza
 # Add a line at the end of your program, outside the for loop, 
