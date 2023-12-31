e8341b41ad6e61314a72007fd99eded61c47001b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:15:28 -0500

    Update personal_message.py

--- ch02/personal_message.py
+++ ch02/personal_message.py
@@ -1,5 +1,5 @@
 # Exercise 2-3: Use a variable to represent a person's name, 
-# and print a message to that person
+# And print a message to that person
 # Your message should be simple, such as, "Hello Eric, would you like to learn some Python today ?"
 
 name = "Jimmy"
