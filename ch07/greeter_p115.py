a25a4340dbf8f76c74344b5c9ea760b0dc4ff2ac
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:15:26 -0500

    Update greeter_p115.py

--- ch07/greeter_p115.py
+++ ch07/greeter_p115.py
@@ -3,6 +3,6 @@
 # Allows you to build your prompt over several lines, and then write a clean input() statement.
 # Use operator += to add a new string onto the end of the string in the previous variable
 prompt = "If you tell us who you are, we can personalize the messages you see." # first line assigns the first part of the message to the variable prompt.
-prompt += "\nWhat is your first name? " # adds the new string to previous string
+prompt += "\nWhat is your first name? " # adds the new string to the previous string
 name = input(prompt) # first assign your prompt to a variable and pass that variable to the input() function.
 print(f"\nHello, {name.title()}!")
