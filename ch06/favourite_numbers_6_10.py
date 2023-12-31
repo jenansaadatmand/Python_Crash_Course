fa20ccaf07fc0226df3ba1b54cf7aa0b38f074f2
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 09:03:53 -0500

    Update favourite_numbers_6_10.py

--- ch06/favourite_numbers_6_10.py
+++ ch06/favourite_numbers_6_10.py
@@ -8,10 +8,11 @@
 # Think of a favourite number for each person
 # and store each as a value in your dictionary
 # Print each person's name and their favourite number
-# for even more fun, poll a few friends and get some actual data for your program
+# For even more fun, poll a few friends and get some actual data for your program
 
 
 # Nesting using a list as value to a key in a dictionary
+
 favourite_numbers = {
     'jimmy': [3, 33, 303], 
     'jim': [100_000, 10_000, 1],
@@ -25,6 +26,7 @@ print(f"Tim's favourite number are: {favourite_numbers['tim']}")
 print(f"Roro's favourite number are: {favourite_numbers['roro']}")
 print(f"Soso's favourite number are: {favourite_numbers['soso']}")
 print()
+
 # Alternative solution
 
 num = favourite_numbers['jimmy']
