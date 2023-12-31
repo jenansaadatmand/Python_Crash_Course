689213c7515019110cc70690917f1cf5d3536f5d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 08:56:01 -0500

    Update hello_admin.py

--- ch05/hello_admin.py
+++ ch05/hello_admin.py
@@ -1,5 +1,5 @@
 # Hello Admin: make a list of five or more usernames
-# includingthe name 'admin'
+# including the name 'admin'
 # Imagine you are writing code that will print a greeting to each user after they log in to a website
 # Loop though the list, and print a greeting to each user:
 # If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
