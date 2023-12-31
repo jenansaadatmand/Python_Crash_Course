a9185a4669b18579cd68aefd27df7431fd688925
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:11:46 -0500

    Update adding_comments.py

--- ch02/adding_comments.py
+++ ch02/adding_comments.py
@@ -1,6 +1,6 @@
 # Exercise 2-11: adding comments Choose two of the programs you've written, 
 # And add at least one comment to each. 
-# if you don't have anything specific to write because your programs are too simple
+# If you don't have anything specific to write because your programs are too simple
 # at this point, just add your name and the current date at the top of each program file.
 # Then write one sentence describing what the program does.  
 
