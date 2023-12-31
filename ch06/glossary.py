76a86fd6a5e8ba0163863287db68a51a28026e4f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 09:04:51 -0500

    Update glossary.py

--- ch06/glossary.py
+++ ch06/glossary.py
@@ -21,7 +21,7 @@ print(f"Dictionary; {glossary['dictionary']}")
 print(f"Constant; {glossary['constant']}")
 print(f"Conditional test; {glossary['conditional test']}")
 print()
-# alternative solution 2
+# Alternative solution 2
 value = glossary['variable']
 print(f"Variable; {value}")
 value = glossary['list']
