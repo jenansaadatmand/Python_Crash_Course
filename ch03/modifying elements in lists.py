dc49215768a55f2189cb84c13cbf906e563fbb78
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:42:00 -0500

    Update modifying elements in lists.py

--- ch03/modifying elements in lists.py
+++ ch03/modifying elements in lists.py
@@ -14,7 +14,7 @@ print()
 # 2. Inserting elements into a list using insert() method
 # 3. Removing elements from a list according to its position in the list using del statement list name[index]
 # 4. Removing element according to its position using the pop() method from any position in the list. useful when you want to use the value of an item after you remove it from a list 
-# 5. removing element by value by remove() method
+# 5. Removing element by value by remove() method
 # organizing a list 
 # 1. sorting permanently with the sort() method
 # 2. sorting a list temporarily with the sorted() function
