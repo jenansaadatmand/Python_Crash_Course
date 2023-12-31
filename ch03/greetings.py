bb79162ee7a510e2a934da6b48c6bb5a2ff04f68
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:39:41 -0500

    Update greetings.py

--- ch03/greetings.py
+++ ch03/greetings.py
@@ -1,5 +1,5 @@
 # Exercise 3-2: Greetings: start with the list you used in exercise 3-1, but
-# instead of just printing each person's name, print a message to them 
+# Instead of just printing each person's name, print a message to them 
 # The text of each message should be the same, but each message should
 # be personalized with the person's name 
 names=['jimmy', 'mimi', 'tony', 'nana']
