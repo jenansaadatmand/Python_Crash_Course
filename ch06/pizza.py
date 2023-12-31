e5dff9fb6c67714bce434fb31f0fcc71af5cdfa3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 09:08:53 -0500

    Update pizza.py

--- ch06/pizza.py
+++ ch06/pizza.py
@@ -4,11 +4,13 @@
 # List of toppings is a value associated with the key 'toppings'
 
 # Store information about a pizza being ordered.
+
 pizza = {
         'crust': 'thick',
         'toppings': ['mushrooms', 'extra cheese'],
         }
 # Summarize the order.
+
 print(f"You ordered a {pizza['crust']}-crust pizza" 
     " with the following toppings:") # breaking a long line and end the line with a quotation mark, indent the next line, add an opening quotation mark and continue the string
 # To print the toppings, we use a for loop and access the dictionary using the key toppings
