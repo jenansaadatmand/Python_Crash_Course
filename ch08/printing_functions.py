ca93e5aa96b882b003044518b115137728c6f61a
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:59:27 -0500

    Update printing_functions.py

--- ch08/printing_functions.py
+++ ch08/printing_functions.py
@@ -12,7 +12,7 @@ def show_completed_models(completed_models): # Define a function with one parame
     """Show all the models that were printed."""
     print("\nThe following models have been printed.")
     for completed_model in completed_models:
-        print(completed_model)   # displays the name of each model that was printed
+        print(completed_model)   # Displays the name of each model that was printed
 
 
 
