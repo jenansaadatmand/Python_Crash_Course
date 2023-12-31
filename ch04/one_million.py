5a4c8986a5432a9d84aa80d20b134e52507cf1e3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 08:37:21 -0500

    Update one_million.py

--- ch04/one_million.py
+++ ch04/one_million.py
@@ -1,5 +1,5 @@
 # Exercise 4-4 One million: make a list of the numbers from one to one million
-# and then use a for loop to print the numbers
+# And then use a for loop to print the numbers
 # (if the output is taking too long, stop it by pressing CTRL-C) or by closing the output window)
 
 #for number in range(1, 1_000_000): # making a list from 1 to 1 million
