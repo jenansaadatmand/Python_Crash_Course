ddcd57b478f56a89cf1949a2ce6827ce74e50b46
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:54:11 -0500

    Update your_own_list.py

--- ch03/your_own_list.py
+++ ch03/your_own_list.py
@@ -2,6 +2,9 @@
 # And make a list that stores several examples
 # Use your list to print a series of statements about these items, such as "I would like to own a Honda motorcycle"
 
+
+# Solution 1:
+
 mode_transport = ['motorcycle', 'car', 'bus', 'train', 'aeroplane', 'ship']
 print(f'I would like to own a Honda {mode_transport[0].title()}')
 print(f'I would like to own a Toyota {mode_transport[1].title()}')
@@ -12,7 +15,8 @@ print(f'I would like to own a crusing {mode_transport[5].title()}')
 print()
 
 
-# Solution 2
+# Solution 2:
+
 mode_transport = ['motorcycle', 'car', 'bus', 'train', 'aeroplane', 'ship']
 msg = 'I would like to own a ' 
 print(f'{msg}{mode_transport[0].title()}')
