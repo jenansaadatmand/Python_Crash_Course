86b724a35b1e1e3a29d2c7146d2a0061704f01e1
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 09:09:45 -0500

    Update user.py

--- ch06/user.py
+++ ch06/user.py
@@ -9,15 +9,18 @@ print(user_0)
 print()
 
 # for loop to print all value to keys
+
 for key in user_0:
     print(user_0[key])
 print()    
 
 # for loop to peint all keys only
+
 for i in user_0:
     print(i)
     
 # Looping and returning all key-value pairs using items() method to iterate over a dictionary
+
 for key, value in user_0.items():
     print(f"\nKey: {key}")
     print(f"Value: {value}")
