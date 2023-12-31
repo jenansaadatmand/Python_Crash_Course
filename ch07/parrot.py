69143a6dba475354ef817daf0d6c85b71cf2e560
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:19:17 -0500

    Update parrot.py

--- ch07/parrot.py
+++ ch07/parrot.py
@@ -1,6 +1,6 @@
 # User input() function, passes the program and waits for the user to enter some text, python assigns the input to a variable 
 # Controlling how long the program runs using a while loop
-# Program asks user to enter some text and displays that message back to the user
+# The program asks user to enter some text and displays that message back to the user
 # Sublime Text and many other editors don’t run programs that prompt the user for input. You can use these editors to write programs that prompt for input, but you’ll need to run these programs from a terminal.
 
 message = input("Tell me something,and I will repeat it back to you:")
