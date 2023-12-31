83d39f19f075b3a90a2a360b324686fa2f167c43
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:14:59 -0500

    Update number_eight.py

--- ch02/number_eight.py
+++ ch02/number_eight.py
@@ -2,7 +2,7 @@
 # Operations that result in the number 8.
 # Be sure to enclose your operations in print() calls to see the results
 # You should create four lines that look like this: print(5+3)
-# Your ouput should be four lines, with the number 8 appearing once on each line
+# Your output should be four lines, with the number 8 appearing once on each line
 # Jenan Saadatmand March 28 2023, this is a comment 
 print(5+3)
 print(16-8)
