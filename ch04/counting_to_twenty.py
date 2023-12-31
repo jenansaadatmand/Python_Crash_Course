5b30626fae11d6095292c1a3def9598537fb4446
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:55:08 -0500

    Update counting_to_twenty.py

--- ch04/counting_to_twenty.py
+++ ch04/counting_to_twenty.py
@@ -1,4 +1,4 @@
-# Exercise 4-3 counting to twenty: Use a for loop to print the number from 1 to 20 inclusive
+# Exercise 4-3 counting to twenty: Use a for loop to print the number from 1 to 20 inclusive.
 
 for x in range(1, 21):
     print(x)
