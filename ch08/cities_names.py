13ff44b16fa681b4b28e08e47cb968bf791be756
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:34:12 -0500

    Update cities_names.py

--- ch08/cities_names.py
+++ ch08/cities_names.py
@@ -1,7 +1,7 @@
 # Cities Names: Write a function called city_country() that takes in the name of a city and its country
 # The function should return a string formatted like this: 
 # " Santiago, Chile"
-# call your function with at least three city-country pairs, and 
+# Call your function with at least three city-country pairs, and 
 # print the values that are returned
 
 def city_country(city, country):
