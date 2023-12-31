c974474fd301bab2a261f423f1bed0502eae3035
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:24:46 -0500

    Update rollercoaster.py

--- ch07/rollercoaster.py
+++ ch07/rollercoaster.py
@@ -1,5 +1,5 @@
 # Program determines whether people are tall enough to ride a roller coaster
-# When you use numerical input to do calculations and comparisons,be sure to convert the input value to a numerical representation first.
+# When you use numerical input to do calculations and comparisons, be sure to convert the input value to a numerical representation first.
 height = input("How tall are you, in inches? ")
 height = int(height)
 if height >= 48:
