2f2fb3e87430bf11a4f5e68ce21b656ee17048bb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:18:16 -0500

    Update strings_flexibility.py

--- ch02/strings_flexibility.py
+++ ch02/strings_flexibility.py
@@ -20,7 +20,7 @@ print(name)
 print(name.title())  # method title() appears after the variable in the print() funtion call, title() method changes each word to title case
 # A method is an action that Python can perform on a piece of data
 # The dot (.) after name in name.title() tells Python to make the title() method act on the variable name
-# Every method is followed by a set of parentheses, because methods often need additional information to do their work. That information is provided inside the parentheses. The title() function doesn’t need any additional information, so its parentheses are empty
+# Every method is followed by a set of parentheses because methods often need additional information to do their work. That information is provided inside the parentheses. The title() function doesn’t need any additional information, so its parentheses are empty
 # Want your program to recognize the input values Ada, ADA, and ada as the same name, and display all of them as Ada.
 
 # Change a string to all uppercase or all lowercase letters
