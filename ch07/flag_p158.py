03d5e60b032617d205062802e5b4960ea7704e8b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:13:23 -0500

    Update flag_p158.py

--- ch07/flag_p158.py
+++ ch07/flag_p158.py
@@ -2,7 +2,7 @@
 # A program in which many different events could cause the program to stop running
 # If many possible events might occur to stop the program, trying to test all these conditions in one while statement becomes complicated and difficult.
 # For a program that should run only as long as many conditions are true, you can define one variable that determines whether or not the entire program is active.
-# This variable is called a flag, acts as a signal to the program 
+# This variable is called a flag, and acts as a signal to the program 
 # Write our programs so they run while the flag is set to True and stop running when any of several events sets the value of the flag to False.
 # As a result, our overall while statement needs to check only one condition: whether or not the flag is currently True.
 # Then, all our other tests (to see if an event has occurred that should set the flag to False) can be neatly organized in the rest of the program.
