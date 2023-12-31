62725bec6f1df9a38d3873c17a0e5d156a666c4e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:36:28 -0500

    Update every_function.py

--- ch03/every_function.py
+++ ch03/every_function.py
@@ -40,7 +40,7 @@ print("Deleting item permanently using del statement:")
 del languages[1]
 print(languages)
 print()
-# Remove items using pop() method from the end of the list and assigning to a variable to be used somewhere else
+# Remove items using pop() method from the end of the list and assign them to a variable to be used somewhere else
 print("Removing item temporarily using pop():")
 popped_lang = languages.pop()
 print("Here is the popped item:")
