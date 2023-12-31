25f65b1e8818f31d9b6e125eee06717b06b1b722
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 08:49:55 -0500

    Update square_numbers.py

--- ch04/square_numbers.py
+++ ch04/square_numbers.py
@@ -1,7 +1,7 @@
 # Square numbers, make a list of the first 10 square numbers 
 squares = []     # Start with an empty list
 for value in range(1, 11):    # Loop through each value from 1 to 10 using the range() function
-    square = value**2 # assigning to a variable & raising the value to the second power and assign to a variable 
+    square = value**2 # Assigning to a variable & raising the value to the second power and assign to a variable 
     squares.append(square)
 print(squares)
 print()
