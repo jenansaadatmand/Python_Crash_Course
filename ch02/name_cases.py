4079b4da60541ef7f6e4aeae3c9dcdcbe87458dd
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:14:08 -0500

    Update name_cases.py

--- ch02/name_cases.py
+++ ch02/name_cases.py
@@ -1,5 +1,5 @@
 # Exercise 2-4 Use a variable to represent a person's name, and then 
-# print that person's name in lowercase, uppercase and title case
+# Print that person's name in lowercase, uppercase and title case
 
 name = "jimmy saadatmand"
 print(name.lower())
