a8b8d6935820130a1ca5799f217541fe070774dc
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 09:06:27 -0500

    Update glossary2.py

--- ch06/glossary2.py
+++ ch06/glossary2.py
@@ -1,6 +1,6 @@
 # Glossar 2: Now that you know how to loop through a dictionary
 # Clean up the code from exercise 6-3 (page 99) by replacing your series of print() calls with a loop that runs through the dictionary's keys and values
-# When you're sure that your loop works, add five more python terms to your glossary
+# When you're sure that your loop works, add five more Python terms to your glossary
 # When you run your program again, these new words and meanings should automatically be included in the output
 
 glossary = {
