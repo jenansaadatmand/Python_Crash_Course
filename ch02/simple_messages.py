7ceb97beaa2c3e2583205a7c8cd733515d666648
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:16:27 -0500

    Update simple_messages.py

--- ch02/simple_messages.py
+++ ch02/simple_messages.py
@@ -1,7 +1,7 @@
 # Exercise 2.2:  Assign a message to a variable,
 # And print that message
 # Then change the value of the variable to a new message
-# and print the new message
+# And print the new message
 message = " I am a successful Data Scientist and a python programmer "
 print(message)
 
