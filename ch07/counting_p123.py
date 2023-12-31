9582ec3b98cc342beec06e763f4f3ca2a42f330d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:09:34 -0500

    Update counting_p123.py

--- ch07/counting_p123.py
+++ ch07/counting_p123.py
@@ -5,6 +5,6 @@
 x = 1
 while x <= 5:
     print(x)
-# This loop runs forever !
+# This loop runs forever!
 # Value of x will start at 1 but never change. As a result, the conditional test x <= 5 will always evaluate to True and the while loop will run forever, printing a series of 1s
     x += 1   # increment by 1 to get out of the infinite loop, But if you accidentally omit the line x += 1 (as shown next), the loop will run forever:
