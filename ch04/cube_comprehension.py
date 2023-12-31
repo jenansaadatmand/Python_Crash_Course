d1889defe8cedc1870b4c0c5466dec74b9f6ba5d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:55:21 -0500

    Update cube_comprehension.py

--- ch04/cube_comprehension.py
+++ ch04/cube_comprehension.py
@@ -1,4 +1,4 @@
 # Exercise 4-9 Cube comprehension: 
-# use a list comprehension to generate a list of the first 10 cubes
+# Use a list comprehension to generate a list of the first 10 cubes
 cubes = [value **3 for value in range(1, 11)]
 print(cubes)
