5f7d27d2bea990811563061eb04e7f7404f92c6b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 17:09:08 -0500

    Update counting_p122.py

--- ch07/counting_p122.py
+++ ch07/counting_p122.py
@@ -2,7 +2,7 @@
 # Use the continue statement to return to the beginning of the loop based on the result of a conditional test.
 # Program with a loop that counts from 1 to 10 but prints only the odd numbers in that range
 
-current_number = 0  # set currency number to 0
+current_number = 0  # Set currency number to 0
 while current_number < 10: # python enters the loop
     current_number += 1  # Increment the count by 1, so the current number is 1
     if current_number % 2 == 0: # If statement checks the modulo of current_number and 2, if modulo is 0 (current_number is divisible by 2)
