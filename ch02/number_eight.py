23667b7a7e470a32cbcd4b288362c1afe4692d58
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 07:14:36 -0500

    Update number_eight.py

--- ch02/number_eight.py
+++ ch02/number_eight.py
@@ -1,5 +1,5 @@
 # Exercise 2-9: Write addition, subtraction, multiplication, and division
-# operations that result in the number 8.
+# Operations that result in the number 8.
 # Be sure to enclose your operations in print() calls to see the results
 # you should create four lines that look like this: print(5+3)
 # your ouput should be four lines, with the number 8 appearing once on each line
