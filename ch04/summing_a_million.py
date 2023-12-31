6e7580b8800d81d92badd5fe02b1f7be7455fc8c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 08:52:04 -0500

    Update summing_a_million.py

--- ch04/summing_a_million.py
+++ ch04/summing_a_million.py
@@ -1,5 +1,5 @@
 # Exercise 4-5 Summing a million: make a list of the numbers from one to one million
-# And then use min() and max() to make sure your list actually starts at one and ends at one million
+# And then use min() and max() to make sure your list starts at one and ends at one million
 # Also, use the sum() function to see how quickly Python can add a million numbers
 
 numbers = list(range(1, 1_000_000))
