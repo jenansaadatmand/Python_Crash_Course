35f6e29616bd7b0283149a8417615aaf7d84a27a
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-01 18:01:48 -0500

    Update alice.py

--- ch10/alice.py
+++ ch10/alice.py
@@ -8,11 +8,11 @@
 # UTF-8 is a variable-width character encoding used for electronic communication. Defined by the Unicode Standard, the name is derived from Unicode (or Universal Coded Character Set) Transformation Format – 8-bit. ... Code points with lower numerical values, which tend to occur more frequently, are encoded using fewer bytes.
 # UTF-8 is the most widely used way to represent Unicode text in web pages, and you should always use UTF-8 when creating your web pages and databases
 # Python cannot read from a missing file, so it raises an exception
-# traceback reports a FileNotFoundError exception is created when Python cannot find the file it's trying to open
+# Traceback reports a FileNotFoundError exception is created when Python cannot find the file it's trying to open
 filename = 'alice.txt'
 
 try:  # The code in the try block produces an error so Python looks for an except block that matches the error
     with open(filename, encoding = 'utf-8'):
         f.read()
-except FileNotFoundError: # python runs the except block to handle the error
-    print(f"sorry, the file {filename} doesn't exist.") # result in friendly error message is printed to the user instead of the traceback
+except FileNotFoundError: # Python runs the except block to handle the error
+    print(f"sorry, the file {filename} doesn't exist.") # Result in friendly error message is printed to the user instead of the traceback
