6ede9ac6d4c8ee6b2c738a8a88e4935eadd29b43
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-01 18:00:51 -0500

    Update alice.py

--- ch10/alice.py
+++ ch10/alice.py
@@ -3,7 +3,7 @@
 # Let's try to read a file that does not exist
 # The program tries to read the contents of Alice in wonderland, but we haven't saved the file alice.txt in the same directory as alice.py
 
-# use of f to represent the file object, common convention
+# Use of f to represent the file object, common convention
 # encoding argument needed when your system's default encoding doesn't match the encoding of the file that's being read
 # UTF-8 is a variable-width character encoding used for electronic communication. Defined by the Unicode Standard, the name is derived from Unicode (or Universal Coded Character Set) Transformation Format – 8-bit. ... Code points with lower numerical values, which tend to occur more frequently, are encoded using fewer bytes.
 # UTF-8 is the most widely used way to represent Unicode text in web pages, and you should always use UTF-8 when creating your web pages and databases
