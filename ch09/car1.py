a97a94be5782fcca64e1bacbec9590902d9cd1f5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 21:02:16 -0500

    Update car1.py

--- ch09/car1.py
+++ ch09/car1.py
@@ -80,7 +80,7 @@ class Car:
             print("You can't roll back an odometer!")
 
 
-my_new_car = Car('audi', 'a4', 2019)  # we cannot add odometer attribute here, it starts at 0
+my_new_car = Car('audi', 'a4', 2019)  # We cannot add odometer attribute here, it starts at 0
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer() 
 
