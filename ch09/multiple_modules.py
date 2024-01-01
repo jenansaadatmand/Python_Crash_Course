8503a3ee0d4c430fcb0a4c399b8b4d1327c39098
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-01 17:41:18 -0500

    Update multiple_modules.py

--- ch09/multiple_modules.py
+++ ch09/multiple_modules.py
@@ -2,4 +2,3 @@
 # And store the Privileges and Admin classes in a separate module
 # In a separate file, create an Admin instance and
 # Call show_privileges() to show that everything is still working correctly
-
