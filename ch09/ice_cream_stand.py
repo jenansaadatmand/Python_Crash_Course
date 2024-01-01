b90a80a84f4b4850315c90c917bf97135f526a06
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-01 16:40:49 -0500

    Update ice_cream_stand.py

--- ch09/ice_cream_stand.py
+++ ch09/ice_cream_stand.py
@@ -1,5 +1,5 @@
 # Ice Cream Stand: An ice cream stand is a specific kind of restaurant
-# Write a class called IceCreamStand that inherits from the Restaurant clas you wrote in Exercise 9-1 (page 162) or Excersize 9-4 (page 167)
+# Write a class called IceCreamStand that inherits from the Restaurant class you wrote in Exercise 9-1 (page 162) or Excersize 9-4 (page 167)
 # Either version of the class will work; just pick the one you like better
 # Add an attribute called flavors that stores a list of ice cream flavors
 # Write a method that displays these flavors
