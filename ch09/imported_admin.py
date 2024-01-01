0b9bc060bdad19cd9bdc28572dae51f84e2486b1
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-01 16:46:36 -0500

    Update imported_admin.py

--- ch09/imported_admin.py
+++ ch09/imported_admin.py
@@ -1,4 +1,4 @@
 # Imported Admin: start with your work from Exercise 9-8 (page 173)
 # Store the classes User, privileges, and Admin in one module
 # Create a separate file, make an Admin instance,
-# and call show_privileges() to show that everything is working correctly.
+# And call show_privileges() to show that everything is working correctly.
