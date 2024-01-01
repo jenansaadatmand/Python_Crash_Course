2b5dcb667a31885dd07167c337c4e2c42486f9b4
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-01 16:38:57 -0500

    Update electric_car_p178.py

--- ch09/electric_car_p178.py
+++ ch09/electric_car_p178.py
@@ -21,7 +21,7 @@ class Battery:  # Define a new class Battery that doesn't inherit from any other
         elif self.battery_size == 100: # If the capacity is 100 kWh, it sets the range to 315 miles, it then reports this value
             range = 315
 
-        print(f"This car can go about {range} miles on a full charge.")   # outcome tells us the range of the car based on its battery size
+        print(f"This car can go about {range} miles on a full charge.")   # Outcome tells us the range of the car based on its battery size
         
 
     def upgrade_battery(self):
