684980271aae991a0e9ec0885ee63025058baf6f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 19:29:17 -0500

    Update admin.py

--- ch09/admin.py
+++ ch09/admin.py
@@ -1,6 +1,6 @@
 # Admin: An administrator is a special kind of user.
 # Write a class called Admin that inherits from the User class you wrote in Exercise 9-3 (page 162) or Exercise 9-5 (page 167)
-# Add an attribute, privileges, that store a list of strings like "can add post", "can delete post", "can ban user", and so on.
+# Add an attribute, and privileges, that store a list of strings like "can add post", "can delete post", "can ban user", and so on.
 # Write a method called show_privileges() that lists the administrator's set of privileges
 # Create an instance of Admin, and call your method
 
