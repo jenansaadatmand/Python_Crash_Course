500d2ba02fd1b82c3bdb8251dcba45bc0fee20f6
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-01 17:40:27 -0500

    Update multiple_modules.py

--- ch09/multiple_modules.py
+++ ch09/multiple_modules.py
@@ -1,5 +1,5 @@
 # Multiple Modules: Store the User class in one module,
-# and store the Privileges and Admin classes in a separate module
+# And store the Privileges and Admin classes in a separate module
 # In a separate file, create an Admin instance and
-# call show_privileges() to show that everything is still working correctly
+# Call show_privileges() to show that everything is still working correctly
 
