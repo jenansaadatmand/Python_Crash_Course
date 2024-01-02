379360f0ead3e0010c8c9a638681a4c42c266205
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-02 10:29:19 -0500

    Update file_reader.py

--- ch10/file_reader.py
+++ ch10/file_reader.py
@@ -14,5 +14,5 @@ print("\n")
 # Reading line by line using a for loop on the file object
 filename = 'text_files/pi_digits.txt' # Assign a variable to the file where are reading from
 with open(filename) as file_object:
-    for line in file_object: # use a for loop to loop through the lines in a file
+    for line in file_object: # Use a for loop to loop through the lines in a file
         print(line.rstrip()) # stripping two blank lines, one invisible newline character at the end of each line from file and one added from print() function recalling
