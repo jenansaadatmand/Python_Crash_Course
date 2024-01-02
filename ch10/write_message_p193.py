668cfe9f41fff1e19fc131f45f43649f78af8faf
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-02 11:16:26 -0500

    Update write_message_p193.py

--- ch10/write_message_p193.py
+++ ch10/write_message_p193.py
@@ -2,6 +2,6 @@
 # Python does not erase the contents of the file before running the file object, lines will be added to the end of the file
 # Modifying write_message.py by adding some new reasons we love programming to the existing file programming.txt
 filename = 'text_files/programming.txt'
-with open(filename, 'a') as file_object:  # use the 'a' argument to open the file for appending rather than writing over the existing file.
+with open(filename, 'a') as file_object:  # Use the 'a' argument to open the file for appending rather than writing over the existing file.
     file_object.write("I also love finding meaning in large datasets.\n")
     file_object.write("I love creating apps that can run in a browser.\n")
