90e7ae4e8ffc578a41fa4c7af6db7e3924e69c22
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-02 10:56:40 -0500

    Update pi_string.py

--- ch10/pi_string.py
+++ ch10/pi_string.py
@@ -6,7 +6,7 @@ filename = 'text_files/pi_digits.txt'
 with open(filename) as file_object: # Open the file 
     lines = file_object.readlines() # Store each line in a list
 pi_string = ''  # Create a variable to hold digits of pi as a list
-for line in lines: # create a loop to add each line of digits to the variable pi_string
+for line in lines: # Create a loop to add each line of digits to the variable pi_string
     pi_string += line.strip() # Remove newline character from each line on the right and left
 print(pi_string) # Print the variable holding a single string
 print(len(pi_string)) # Print the length of the variable
