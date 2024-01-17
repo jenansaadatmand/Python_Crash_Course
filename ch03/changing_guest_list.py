85ad78ebb16430f1e38cd156704c36be56fab99c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-17 10:51:44 -0500

    Update changing_guest_list.py

--- ch03/changing_guest_list.py
+++ ch03/changing_guest_list.py
@@ -1,6 +1,6 @@
 # Exercise 3-5: Changing Guest List: you just heard that one of your guests can't make the dinner
 # So you need to send out a new set of invitations. you"'ll have to think of someone else to invite
-# Start with your program from exercise 3-4.
+# Start with your program from exercises 3-4.
 # Add a print() call at the end of your program stating the name of the guest who can't make it
 # Modify your list, replacing the name of the guest who can't make it with the name of the new person you are inviting
 # Print a second set of invitation messages, one for each person who is still in your list
