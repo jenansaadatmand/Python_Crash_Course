d05ea5497c3485eed54fb66b03bcee9864aa1998
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-23 15:04:47 -0500

    Update file_extensions.py

--- ch02/file_extensions.py
+++ ch02/file_extensions.py
@@ -1,6 +1,6 @@
 # Exercise 2-8: Python has a removesuffix() method that works exactly like 
 # removeprefix()
-# Assign the value 'python_notes.txt' to a variable calle filename
+# Assign the value 'python_notes.txt' to a variable called filename
 # Then use the removesuffix() method to display the filename without the file extension
 # like some file browsers do
 
