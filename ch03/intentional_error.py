f77c680fb943c963f96d9045a3106717048301d1
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-22 19:55:31 -0500

    Update intentional_error.py

--- ch03/intentional_error.py
+++ ch03/intentional_error.py
@@ -1,4 +1,4 @@
-# Intentional Error: if you haven't recieved error in one of your programs yet, try to make one happen.  
+# Intentional Error: if you haven't received error in one of your programs yet, try to make one happen.  
 # Change an index in one of your programs to produce an index error
 # Make sure you correct the error before closing the program
 
