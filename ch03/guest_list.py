3219a207d2e303caf205433d7b7c9929c527f772
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-24 20:22:21 -0500

    Update guest_list.py

--- ch03/guest_list.py
+++ ch03/guest_list.py
@@ -9,7 +9,8 @@ print(f"Hello {guest_list[1].title()}, you are invited to dinner.")
 print(f"Hello {guest_list[2].title()}, you are invited to dinner.")
 print()
 
-# Solution # 2
+# Solution # 2:
+
 # Using a for loop
 for guest in guest_list:
     print(f"Hello {guest.title()}, you are invited to dinner.")
