8f34a8b3714fd2b25d2748fbc122e87323772497
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-24 20:14:35 -0500

    Update your_own_list.py

--- ch03/your_own_list.py
+++ ch03/your_own_list.py
@@ -1,4 +1,4 @@
-# Think of your favourite mode of transportation, such as a motorcycle or a car,
+# Think of your favorite mode of transportation, such as a motorcycle or a car,
 # And make a list that stores several examples
 # Use your list to print a series of statements about these items, such as "I would like to own a Honda motorcycle"
 
