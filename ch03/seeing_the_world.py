132560695b510b5cd9ae2c08970bcf45c05577f5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-24 20:16:52 -0500

    Update seeing_the_world.py

--- ch03/seeing_the_world.py
+++ ch03/seeing_the_world.py
@@ -40,7 +40,7 @@ print(places)
 # Use sorted() to print your list in reverse-alphabetical order without changing the order of the original list
 # Show that your list is still in its original order by printing it again
 # Use reverse() to change the order of your list. print the list to show that its order has changed
-# Use reverse() to change the order of your list again. Print the list to show its back to its original order
+# Use reverse() to change the order of your list again. Print the list to show it back to its original order
 # Use sort() to change your list so it's stored in alphabetical order.
 # Print the list to show that its order has changed
 # Use sort() to change your list so it's stored in reverse-alphabetical order.
