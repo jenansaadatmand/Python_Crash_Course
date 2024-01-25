427b388f8c3708e1a81864bfdde3a094ef1ddac5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-24 20:24:19 -0500

    Update changing_guest_list.py

--- ch03/changing_guest_list.py
+++ ch03/changing_guest_list.py
@@ -3,7 +3,7 @@
 # Start with your program from exercises 3-4.
 # Add a print() call at the end of your program stating the name of the guest who can't make it
 # Modify your list, replacing the name of the guest who can't make it with the name of the new person you are inviting
-# Print a second set of invitation messages, one for each person who is still in your list
+# Print a second set of invitation messages, one for each person who is still on your list
 
 guest_list = ['jimmy', 'tiffany', 'spoiler']
 print(guest_list)
