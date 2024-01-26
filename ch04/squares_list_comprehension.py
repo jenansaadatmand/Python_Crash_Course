88a913d9b111b0525d699eb8cb35db361fc75497
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-26 18:32:35 -0500

    Update squares_list_comprehension.py

--- ch04/squares_list_comprehension.py
+++ ch04/squares_list_comprehension.py
@@ -1,4 +1,4 @@
-# list comprehension of squares.py used instead of long code multistep code using range() and for loop and append method to make list numbers  
+# List comprehension of squares.py used instead of long code multistep code using range() and for loop and append method to make list numbers  
 # List comprehension in one line of code, it combines for loop and the creation of new elements into one line and automatically appends each new element
 # Square numbers are presented as value ** 2
 
