5398979ca198b19d62f7db5d9d26afc35e16b7aa
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-27 13:37:14 -0500

    Update stages_of_life.py

--- ch05/stages_of_life.py
+++ ch05/stages_of_life.py
@@ -4,7 +4,7 @@
 # If the person is at least 2 years old but less than 4, print a message that the person is a toddler
 # If the person is at least 4 years old but less than 13, print a message that the person is a kid
 # If the person is at leasdt 13 years old but less than 20, print a message that the person is a teenager
-# If the person is at least 20 years old but less than 65, print a message that the peron is an adult
+# If the person is at least 20 years old but less than 65, print a message that the person is an adult
 # IF the person is age 65 or older, print a message that the person is an elder
 
 age = 17
