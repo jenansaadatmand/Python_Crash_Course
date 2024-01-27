b15d8c8b3acf0b1dd92ac441555611519008f4d8
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-27 13:32:11 -0500

    Update alien_colors_3.py

--- ch05/alien_colors_3.py
+++ ch05/alien_colors_3.py
@@ -1,4 +1,5 @@
-# Alien colors # 3: Turn your if-else chain from exercise 5-4 into an if-elif-else chain
+# Alien colors # 3: 
+# Turn your if-else chain from exercise 5-4 into an if-elif-else chain
 # If the alien is green, print a message that the player earned 5 points
 # If the alien is yellow, print a message that the player earned 10 points
 # If the alien is red, print a message that the player earned 15 points
