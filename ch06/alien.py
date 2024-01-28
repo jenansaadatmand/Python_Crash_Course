31073d2c1dfd1b7c81e5e26db7df1c8ace7e3128
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-28 00:13:28 -0500

    Update alien.py

--- ch06/alien.py
+++ ch06/alien.py
@@ -10,7 +10,7 @@ print("\n")
 
 # Using the x and y coordinates of the alien, place the alien in a particular
 # Position on left edge of the screen, 25 pixels down from the top
-# Screen coordinates usually start at the upper-left corner of screen
+# Screen coordinates usually start at the upper-left corner of the screen
 # We will place alien on the left edge by setting the X-coordinate to 0 and 25 pixels from the top
 # By setting its Y-coordinate to positive 25
 alien_0['x_position'] = 0 # adding new key-value pais
