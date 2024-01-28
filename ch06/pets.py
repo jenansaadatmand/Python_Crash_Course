ed3890e198c8fde6b854a1bbd8a0072c8b7f7f21
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-28 00:25:56 -0500

    Update pets.py

--- ch06/pets.py
+++ ch06/pets.py
@@ -1,7 +1,7 @@
 # Pets: Make several dictionaries, where each dictionary represents a different pet.
 # In each dictionary, include the kind of animal and the owner's name.
 # Store these dictionaries in a list called pets
-# Next, loop through your list and as you do, print everything in a list called pets
+# Next, loop through your list, and as you do, print everything in a list called pets
 # Next, loop through your list and as you do, print everything you know about each pet
 
 pets = []
