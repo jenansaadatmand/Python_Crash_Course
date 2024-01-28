428da25beab366b541614b93d405846464fa602e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-28 18:55:07 -0500

    Update age_p115.py

--- ch07/age_p115.py
+++ ch07/age_p115.py
@@ -3,6 +3,6 @@
 # The program asks for the user's age
 # int() function, treat the input as a numerical value.
 age = input("How old are you? ")
-age = int(age) # converts input string to a numerical value
+age = int(age) # Converts input string to a numerical value
 print(age) # python returns a string "number", representing the numerical value entered
 print(f"You are {age} years old!")
