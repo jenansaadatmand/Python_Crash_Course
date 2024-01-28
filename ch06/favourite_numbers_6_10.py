4bf42b42d47e5c3ec36642774d0b510d6784a75b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-28 00:22:09 -0500

    Update favourite_numbers_6_10.py

--- ch06/favourite_numbers_6_10.py
+++ ch06/favourite_numbers_6_10.py
@@ -6,7 +6,7 @@
 # 6-2 page 99 Favourite numbers: use a dictionary to store people's favourite numbers
 # Think of five names, and use them as keys in your dictionary
 # Think of a favourite number for each person
-# and store each as a value in your dictionary
+# And store each as a value in your dictionary
 # Print each person's name and their favourite number
 # For even more fun, poll a few friends and get some actual data for your program
 
