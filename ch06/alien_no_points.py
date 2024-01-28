80669275b219a9292937351065ccac5d4e7844bb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-28 00:14:27 -0500

    Update alien_no_points.py

--- ch06/alien_no_points.py
+++ ch06/alien_no_points.py
@@ -2,7 +2,7 @@
 alien_0 = {'color': 'green', 'speed': 'slow'}
 # Print(alien_0['points'])  # error due to key does not exist
 # Use get() method to set a value that will be returned if the requested key does not exist
-# Handling error of abscence of key in the dictionary using get()
+# Handling error of absence of key in the dictionary using get()
 # Dictionary name.get('key', 'associated value') returns a clean message instead of an error
 point_value = alien_0.get('points', 'No point value assigned.')
 print(point_value)
