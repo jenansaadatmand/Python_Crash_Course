95be3ab69272f828bce49114d7c30673ea695852
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-28 00:16:47 -0500

    Update cities.py

--- ch06/cities.py
+++ ch06/cities.py
@@ -1,6 +1,6 @@
 # 6_11 Cities: make a dictionary called cities
 # Use the names of three cities as keys in your dictionary
-# Create a dictionary of information about each city and include the country that the city is in, its approximate population and one fact about the that city
+# Create a dictionary of information about each city and include the country that the city is in, its approximate population and one fact about that city
 # The keys for each city's dictionary should be something like country, population, and fact.
 # Print the name of each city and all of the information you have stored about it
 
