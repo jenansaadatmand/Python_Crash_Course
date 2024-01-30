ac7dd94dea05d843845976646e8b9c74a0461dfe
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-30 16:44:49 -0500

    Update greet_users.py

--- ch08/greet_users.py
+++ ch08/greet_users.py
@@ -4,7 +4,7 @@
 # We have a list of users, and we want to print  a greeting to each
 # The program sends a personalized greeting to each user by sending a list of names to a function called greet_users(), which greets each person in the list individually
 
-def greet_users(names): # define the function to expect a list of names, which it assigns to a parameter names
+def greet_users(names): # Define the function to expect a list of names, which it assigns to a parameter names
     """print a simple greeting to each user in the list."""
     for name in names: # function loops through the list it receives 
         msg = f"Hello, {name.title()}!" 
