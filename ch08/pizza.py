5b8ea8badeffd24e00259a26bffb611f667452db
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-30 16:54:57 -0500

    Update pizza.py

--- ch08/pizza.py
+++ ch08/pizza.py
@@ -4,7 +4,7 @@
 # A function that builds pizza. it needs to accept a number of toppings, but you can't know ahead of time how many topping a person will want
 # The function has one parameter *toppings, but this parameter collects as many arguments as the calling line provides.
 # The asterisk * in the parameter tells Python to create an empty tuple called toppings and pack whatever values it receives into this tupple
-# The printed output is a tupple even if the function receives one argument only eg:('pepperoni',)
+# The printed output is a tuple even if the function receives one argument only eg:('pepperoni',)
 
 #def make_pizza(*toppings):  # The * asterisk allows the function to accept arbitrary number of parameters that allow passing an arbitrary number of arguments 
 #    """Print the list of toppings that have been requested."""
