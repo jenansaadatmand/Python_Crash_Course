c5334521a2537d7f1239bbaf8d005be82b30ba6f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-30 16:53:19 -0500

    Update pets.py

--- ch08/pets.py
+++ ch08/pets.py
@@ -41,7 +41,7 @@ print()
 # A dog named willie
 describe_pet('willie')
 describe_pet(pet_name='willie')
-# A hamster nammed harry
+# A hamster named harry
 describe_pet('harry', 'hamster')
 describe_pet(pet_name='harry', animal_type='hamster')
 describe_pet(animal_type='hamster', pet_name='harry')
