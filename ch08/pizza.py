2c22a354672e498f8f2bc69b234e1d1669c37492
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-30 16:53:56 -0500

    Update pizza.py

--- ch08/pizza.py
+++ ch08/pizza.py
@@ -3,7 +3,7 @@
 # Python allows a function to collect an arbitrary number of arguments from calling statement
 # A function that builds pizza. it needs to accept a number of toppings, but you can't know ahead of time how many topping a person will want
 # The function has one parameter *toppings, but this parameter collects as many arguments as the calling line provides.
-# The asterick * in the parameter tells python to create an empty tupple called toppings and pack whatever values it recieves into this tupple
+# The asterisk * in the parameter tells python to create an empty tupple called toppings and pack whatever values it recieves into this tupple
 # The printed output is a tupple even if the function recieves one argument only eg:('pepperoni',)
 
 #def make_pizza(*toppings):  # The * asterick allows the function to accept arbitrary number of parameters that allow passing an arbitrary number of arguments 
