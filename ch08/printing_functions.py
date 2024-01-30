b8f74685f5a445dad7d42a4e3a152e2c6b638931
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-30 16:56:45 -0500

    Update printing_functions.py

--- ch08/printing_functions.py
+++ ch08/printing_functions.py
@@ -1,6 +1,6 @@
 # Defining and printing functions
 def print_models(unprinted_designs, completed_models): # Define a function with two parameters, a list of designs that need to be printed, and a list of completed models.
-    """simulate printing each desing, until none are left. 
+    """simulate printing each design, until none are left. 
        Move each design to completed_models after printing.""" # Description of the function is well-documented in a docstring
     while unprinted_designs:
         current_design = unprinted_designs.pop() # Emptying the list of unprinted_designs from end and fills current_designs, then adds/fills current_designs to completed_models 
