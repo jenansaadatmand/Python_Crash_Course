cf3a5ceab9bc63efe6463c12ef0e570f9a3529dc
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-30 16:56:01 -0500

    Update printing_functions.py

--- ch08/printing_functions.py
+++ ch08/printing_functions.py
@@ -1,4 +1,4 @@
-
+# Defining and printing functions
 def print_models(unprinted_designs, completed_models): # Define a function with two prameters, a list of designs that need to be printed, and a list of completed models.
     """simulate printing each desing, until none are left. 
        Move each design to completed_models after printing.""" # Description of the function is well-documented in a docstring
