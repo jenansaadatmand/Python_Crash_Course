dcc7763cd1cd5413bb612723510bc239dc79d170
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-31 13:43:49 -0500

    Update dice.py

--- ch09/dice.py
+++ ch09/dice.py
@@ -2,7 +2,7 @@
 # Which has a default value of 6
 # Write a method called roll_die() 
 # That prints a random number between 1 and the number of sides the die has
-# Make a 6 sided die and roll it 10 times
+# Make a 6-sided die and roll it 10 times
 
 
 from random import randint
