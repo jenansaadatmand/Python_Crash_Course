0a47e18f59f9bd7028ace414181778bc0495392a
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-03 14:57:01 -0500

    Add files via upload

--- ch10/write_message_p193.py
+++ ch10/write_message_p193.py
@@ -1,7 +1,7 @@
-# Append mode: 'a' is used when you want to add content to a file instead of writing over existing content.
-# Python does not erase the contents of the file before running the file object, lines will be added to the end of the file
-# Modifying write_message.py by adding some new reasons we love programming to the existing file programming.txt
+# append mode: 'a' is used when you want to add content to a file instead of writing over existing content.
+# python does not erase the contents of the file before running the file object, lines will be added to the end of the file
+# modifying write_message.py by adding some new reasons we love programming to the existing file programming.txt
 filename = 'text_files/programming.txt'
-with open(filename, 'a') as file_object:  # Use the 'a' argument to open the file for appending rather than writing over the existing file.
+with open(filename, 'a') as file_object:  # use the 'a' argument to open the file for appending rather than writing over the existing file.
     file_object.write("I also love finding meaning in large datasets.\n")
     file_object.write("I love creating apps that can run in a browser.\n")
