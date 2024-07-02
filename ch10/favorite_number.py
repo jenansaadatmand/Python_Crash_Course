8e083499b49ce02e90ed45f178949c36f07ef5fa
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-07-02 00:10:26 -0400

    Update favorite_number.py

--- ch10/favorite_number.py
+++ ch10/favorite_number.py
@@ -21,3 +21,10 @@ with open(filename) as f:
 print(f"I know your favorite number! it's {num}.")
 
 # If you forgot the f before " and it did not replace the number in line 21 when printed
+
+
+
+
+
+
+
