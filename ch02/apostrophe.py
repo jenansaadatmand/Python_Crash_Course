947da21c571e695dc821d2916262898449e7fba0
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-11-28 16:35:45 -0500

    Update apostrophe.py

--- ch02/apostrophe.py
+++ ch02/apostrophe.py
@@ -10,3 +10,7 @@ print(message)
 # Syntax error: if you use single quotes, python can't identify where the string should end
 # message = 'One of Python's strengths is its diverse community.'
 # print(message)
+
+
+
+
