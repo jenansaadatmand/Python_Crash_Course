19193a82013b55023103a75e72ab95361838516d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2025-01-21 20:23:47 -0500

    Update apostrophe.py

--- ch02/apostrophe.py
+++ ch02/apostrophe.py
@@ -10,3 +10,5 @@ print(message)
 # Syntax error: if you use single quotes, python can't identify where the string should end
 # message = 'One of Python's strengths is its diverse community.'
 # print(message)
+
+
