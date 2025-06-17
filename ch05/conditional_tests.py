47b89cd1d91a8920bf85032dca23862a411d5817
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2025-06-17 00:12:50 -0400

    Update conditional_tests.py

--- ch05/conditional_tests.py
+++ ch05/conditional_tests.py
@@ -29,3 +29,5 @@ print("\nmotorcycle == 'haya busa' ? I predict False.")
 print(motorcycle == 'haya busa')
 print("motorcycle != 'haya busa' ? I predict True.")
 print(motorcycle != 'haya busa')
+
+
