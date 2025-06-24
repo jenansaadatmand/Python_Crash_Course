dda39fe176624e0a2d5826c86ff3adf0302f38b4
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2025-06-23 23:33:36 -0400

    Update famous_quote_2.py

--- ch02/famous_quote_2.py
+++ ch02/famous_quote_2.py
@@ -4,3 +4,4 @@
 famous_person = "albert einstein"
 message = "The greatest glory in living lies not in never falling, but in rising every time we fall."
 print(f'{famous_person.title()} once said, "{message}"')
+
