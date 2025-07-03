3a3839365efdae828cd7d4f6704aa34bc252d72b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2025-07-02 21:38:42 -0400

    Update hello_admin.py

--- ch05/hello_admin.py
+++ ch05/hello_admin.py
@@ -13,4 +13,3 @@ for username in usernames:
         print(f"Hello {username.title()}, would you like to see a status report?")
     else: 
         print(f"Hello {username.title()}, thank you for logginh in again!")
-
