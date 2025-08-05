91de723a35e69b9f88f616d1bc1857164bd2efea
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2025-08-04 21:09:42 -0400

    Update banned_users.py

--- ch05/banned_users.py
+++ ch05/banned_users.py
@@ -2,6 +2,5 @@ banned_users = ['andrew', 'carolina', 'david']
 user = 'marie'
 if user not in banned_users:
     print(f"{user.title()}, you can post if you wish.")
-    
 else:
     print('You cannot post.')
