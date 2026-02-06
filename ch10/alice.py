0c34918aedf23d08d2eb56f770a9e9bbe1d00592
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2026-02-05 19:33:39 -0500

    Update alice.py

--- ch10/alice.py
+++ ch10/alice.py
@@ -11,6 +11,7 @@
 # Traceback reports a FileNotFoundError exception is created when Python cannot find the file it's trying to open
 filename = 'alice.txt'
 
+
 try:  # The code in the try block produces an error so Python looks for an except block that matches the error
     with open(filename, encoding = 'utf-8'):
         f.read()
