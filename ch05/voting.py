2ec13f7b4dad44c91f982c7612da99c5c0660254
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2026-02-13 18:18:08 -0500

    Update voting.py

--- ch05/voting.py
+++ ch05/voting.py
@@ -5,6 +5,7 @@ if age >= 18:
     print("You are old enough to vote !")
     print("HAve you registered to vote yet?")
 
+
 else: 
     print("Sorry, you are too young to vote.")
     print("Please register to vote as soon as you turn 18!")
