2f9c6e8df0fca0b9f990e3ee00a859a079f0d05f
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2026-02-25 23:40:23 -0500

    Update even_or_odd.py

--- ch07/even_or_odd.py
+++ ch07/even_or_odd.py
@@ -5,7 +5,6 @@
 # Even numbers are always divisible by two, so if the modulo of a number and two is zero (here, if number % 2 == 0) the number is even. Otherwise, it’s odd.
 # programs guess the number is even or odd based on % and 0 remainder
 
-
 number = input("Enter a number, and I'll tell you if it's even or odd: ")
 number = int(number)
 if number % 2 == 0:
